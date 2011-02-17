361581b408f1e7030de58becd43c2bf6737e9513
Matt Gormley <mrg@cs.jhu.edu>
2011-02-17 20:37:51 +0000

    Refactoring encode
    git-svn-id: svn+ssh://external.hltcoe.jhu.edu/home/hltcoe/mgormley/public/repos/dep_parse_filtered/trunk@31 0408881f-cb4b-4750-8b71-c94bcb194269

--- src/edu/jhu/hltcoe/parse/IlpViterbiParser.java
+++ src/edu/jhu/hltcoe/parse/IlpViterbiParser.java
@@ -55,63 +55,76 @@ public class IlpViterbiParser implements ViterbiParser {
     private String encode(File tempDir, Sentence sentence, Model model) {
         try {
             // Encode sentence
-            File sentFile = new File(tempDir, "input.sent");
-            PrintWriter sentWriter = new PrintWriter(sentFile);
-            sentWriter.format("0 %d %s\n", WALL_POSITION, WALL_ID);
-            for (int i=0; i<sentence.size(); i++) {
-                Label label = sentence.get(i); 
-                // Must add one to each word position
-                sentWriter.format("0 %d \"%s\"\n", i+1, label.getLabel());
-            }
-            sentWriter.close();
+            encodeSentence(tempDir, sentence);
             
             // Encode model 
             //TODO: handle more than just the DMV
             DmvModel dmv = (DmvModel)model;
-            File stopWeightsFile = new File(tempDir, "input.stopweights");
-            PrintWriter stopWeightsWriter = new PrintWriter(stopWeightsFile);
-            Map<Triple<Label,String,Boolean>,Double> stopWeights = dmv.getStopWeights();
-            for (Entry<Triple<Label,String,Boolean>,Double> entry : stopWeights.entrySet()) {
-                Label label = entry.getKey().get1();
-                String leftRight = entry.getKey().get2();
-                int adjacent = entry.getKey().get3() ? 1 : 0;
-                double weight = entry.getValue();
-                stopWeightsWriter.format("\"%s\" %s %d %f\n", label.getLabel(), leftRight, adjacent, weight);
-            }
-            stopWeightsWriter.close();
-            
-            File chooseWeightsFile = new File(tempDir, "input.chooseweights");
-            PrintWriter chooseWeightsWriter = new PrintWriter(chooseWeightsFile);
-            Map<Pair<Label,Label>,Double> chooseWeights = dmv.getChooseWeights();
-            for (Entry<Pair<Label,Label>,Double> entry : chooseWeights.entrySet()) {
-                Label parent = entry.getKey().get1();
-                Label child = entry.getKey().get2();
-                double weight = entry.getValue();
-                chooseWeightsWriter.format("\"%s\" \"%s\" %f\n", parent.getLabel(), child.getLabel(), weight);
-            }
-            chooseWeightsWriter.close();
+            encodeDmv(tempDir, dmv);
             
             // Create .zpl file
-            String zimplFile = new File(tempDir, "parse.zpl").getAbsolutePath();
-            PrintWriter zimplWriter;
-            zimplWriter = new PrintWriter(zimplFile);
-            zimplWriter.write(codeMap.get("setup"));
-            zimplWriter.write(codeMap.get("deptree-general"));
-            if (projective) {
-                zimplWriter.write(codeMap.get("deptree-proj"));
-            } else {
-                zimplWriter.write(codeMap.get("deptree-nonproj"));
-            }
-            zimplWriter.write(codeMap.get("dmv-objective"));
-            zimplWriter.close();
+            File zimplFile = createZimplFile(tempDir);
             
-            return zimplFile;
+            return zimplFile.getAbsolutePath();
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
 
+    private File createZimplFile(File tempDir) throws FileNotFoundException {
+        File zimplFile = new File(tempDir, "parse.zpl");
+        PrintWriter zimplWriter;
+        zimplWriter = new PrintWriter(zimplFile);
+        zimplWriter.write(codeMap.get("setup"));
+        zimplWriter.write(codeMap.get("deptree-general"));
+        if (projective) {
+            zimplWriter.write(codeMap.get("deptree-proj"));
+        } else {
+            zimplWriter.write(codeMap.get("deptree-nonproj"));
+        }
+        zimplWriter.write(codeMap.get("dmv-objective"));
+        zimplWriter.close();
+        return zimplFile;
+    }
+
+    private void encodeSentence(File tempDir, Sentence sentence) throws FileNotFoundException {
+        File sentFile = new File(tempDir, "input.sent");
+        PrintWriter sentWriter = new PrintWriter(sentFile);
+        sentWriter.format("0 %d %s\n", WALL_POSITION, WALL_ID);
+        for (int i=0; i<sentence.size(); i++) {
+            Label label = sentence.get(i); 
+            // Must add one to each word position
+            sentWriter.format("0 %d \"%s\"\n", i+1, label.getLabel());
+        }
+        sentWriter.close();
+    }
+
+    private void encodeDmv(File tempDir, DmvModel dmv) throws FileNotFoundException {
+        File stopWeightsFile = new File(tempDir, "input.stopweights");
+        PrintWriter stopWeightsWriter = new PrintWriter(stopWeightsFile);
+        Map<Triple<Label,String,Boolean>,Double> stopWeights = dmv.getStopWeights();
+        for (Entry<Triple<Label,String,Boolean>,Double> entry : stopWeights.entrySet()) {
+            Label label = entry.getKey().get1();
+            String leftRight = entry.getKey().get2();
+            int adjacent = entry.getKey().get3() ? 1 : 0;
+            double weight = entry.getValue();
+            stopWeightsWriter.format("\"%s\" %s %d %f\n", label.getLabel(), leftRight, adjacent, weight);
+        }
+        stopWeightsWriter.close();
+        
+        File chooseWeightsFile = new File(tempDir, "input.chooseweights");
+        PrintWriter chooseWeightsWriter = new PrintWriter(chooseWeightsFile);
+        Map<Pair<Label,Label>,Double> chooseWeights = dmv.getChooseWeights();
+        for (Entry<Pair<Label,Label>,Double> entry : chooseWeights.entrySet()) {
+            Label parent = entry.getKey().get1();
+            Label child = entry.getKey().get2();
+            double weight = entry.getValue();
+            chooseWeightsWriter.format("\"%s\" \"%s\" %f\n", parent.getLabel(), child.getLabel(), weight);
+        }
+        chooseWeightsWriter.close();
+    }
+    
     private DepTree decode(Sentence sentence, Map<String, String> result) {
         int[] parents = new int[sentence.size()];
         for (Entry<String,String> entry : result.entrySet()) {
