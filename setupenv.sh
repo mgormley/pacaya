19656609368ed21f3ba0e7f383a5aadabb120d34
Matt Gormley <mrg@cs.jhu.edu>
2011-02-21 04:30:22 +0000

    Updating setupenv
    git-svn-id: svn+ssh://external.hltcoe.jhu.edu/home/hltcoe/mgormley/public/repos/dep_parse_filtered/trunk@37 0408881f-cb4b-4750-8b71-c94bcb194269

--- setupenv.sh
+++ setupenv.sh
@@ -1,5 +1,5 @@
 ROOT_DIR=`pwd`
 #ROOT_DIR=`dirname $0`
-export CLASSPATH=$ROOT_DIR/classes:$ROOT_DIR/lib/*.jar
+export CLASSPATH=$ROOT_DIR/classes:$ROOT_DIR/lib/*.jar:/Library/gurobi301/mac64/lib/gurobi.jar
 export PYTHONPATH=$ROOT_DIR/scripts
 export PATH=$ROOT_DIR/bin:$PATH
