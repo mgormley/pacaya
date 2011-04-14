0dbfdd372fa70dddf85bdaefd56bd2d9eeed6a26
Matt Gormley <mrg@cs.jhu.edu>
2011-04-14 22:26:02 +0000

    Adding run script for DP and moving flow
    git-svn-id: svn+ssh://external.hltcoe.jhu.edu/home/hltcoe/mgormley/public/repos/dep_parse_filtered/trunk@51 0408881f-cb4b-4750-8b71-c94bcb194269

---
+++ scripts/zimpl/run_dp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Be sure to source setupenv.sh before running to get zimpl on the path
+#
+
+zimpl-3.1.0.darwin.x86.gnu.opt -v5 parse_dp.zpl 
+
+if [[ $? != 0 ]] ;
+then echo "Error"; exit 1;
+fi
+
+gurobi_cl ResultFile=parse_dp.sol parse_dp.lp 
