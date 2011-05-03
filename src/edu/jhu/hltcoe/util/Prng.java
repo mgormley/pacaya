f9c4b19a2d76dceaff09b81ebc4bfe5e1def1275
Matt Gormley <mrg@cs.jhu.edu>
2011-05-03 15:11:36 +0000

    Switching to time as random seed
    git-svn-id: svn+ssh://external.hltcoe.jhu.edu/home/hltcoe/mgormley/public/repos/dep_parse_filtered/trunk@59 0408881f-cb4b-4750-8b71-c94bcb194269

--- src/edu/jhu/hltcoe/util/Prng.java
+++ src/edu/jhu/hltcoe/util/Prng.java
@@ -3,8 +3,14 @@ package edu.jhu.hltcoe.util;
 import java.util.Random;
 
 public class Prng {
-
-    public static final int DEFAULT_SEED = 195982758;
     
+    public static final long DEFAULT_SEED;
+
+    static {
+//        DEFAULT_SEED = 195982758;
+        DEFAULT_SEED = System.currentTimeMillis();
+        System.out.println("DEFAULT_SEED="+DEFAULT_SEED);
+    }
+        
     public static final Random random = new Random(DEFAULT_SEED);
 }
