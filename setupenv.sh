e2859484c9b4ec06033524766461743dd7e05a3c
Matt Gormley <mrg@cs.jhu.edu>
2011-06-22 18:05:08 +0000

    Updating build.xml and environment setup
    git-svn-id: svn+ssh://external.hltcoe.jhu.edu/home/hltcoe/mgormley/public/repos/dep_parse_filtered/trunk@110 0408881f-cb4b-4750-8b71-c94bcb194269

--- setupenv.sh
+++ setupenv.sh
@@ -1,5 +1,6 @@
 ROOT_DIR=`pwd`
 #ROOT_DIR=`dirname $0`
-export CLASSPATH=$ROOT_DIR/classes:$ROOT_DIR/lib/*:/Library/gurobi301/mac64/lib/gurobi.jar:/home/hltcoe/mgormley/working/tagging/bin/gurobi301/linux64/lib/gurobi.jar
+#export CLASSPATH=$ROOT_DIR/classes:$ROOT_DIR/lib/*:/Library/gurobi301/mac64/lib/gurobi.jar:/home/hltcoe/mgormley/working/tagging/bin/gurobi301/linux64/lib/gurobi.jar
+export CLASSPATH=$ROOT_DIR/classes:$ROOT_DIR/lib/*:/home/hltcoe/mgormley/working/tagging/bin/gurobi301/linux64/lib/gurobi.jar:/home/hltcoe/mgormley/installed/ILOG/CPLEX_Studio_AcademicResearch122/cplex/lib/cplex.jar
 export PYTHONPATH=$ROOT_DIR/scripts
 export PATH=$ROOT_DIR/bin:$PATH
