39ec8f230391dec014e2a58660d4389c0b0a865c
Matt Gormley <mrg@cs.jhu.edu>
2011-06-25 22:03:17 +0000

    updates to imports
    git-svn-id: svn+ssh://external.hltcoe.jhu.edu/home/hltcoe/mgormley/public/repos/dep_parse_filtered/trunk@123 0408881f-cb4b-4750-8b71-c94bcb194269

--- scripts/experiments/scrape_expout.py
+++ scripts/experiments/scrape_expout.py
@@ -9,11 +9,11 @@ import tempfile
 import stat
 import subprocess
 from optparse import OptionParser
-from experiments.run_experiments import DPExpParams
+from run_experiments import DPExpParams
 from glob import glob
-from experiments.core.util import get_all_following, get_following, get_time,\
+from core.util import get_all_following, get_following, get_time,\
     to_str
-from experiments.core.scrape import Scraper
+from core.scrape import Scraper
 
 class DPScraper(Scraper):
     
