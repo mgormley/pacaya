ce450747fdea8d37380442ad6777f8adea3db078
Matt Gormley <mrg@cs.jhu.edu>
2011-06-27 01:20:46 +0000

    Updating PYTHONPATH
    git-svn-id: svn+ssh://external.hltcoe.jhu.edu/home/hltcoe/mgormley/public/repos/dep_parse_filtered/trunk@126 0408881f-cb4b-4750-8b71-c94bcb194269

--- setupenv.sh
+++ setupenv.sh
@@ -2,5 +2,5 @@ ROOT_DIR=`pwd`
 #ROOT_DIR=`dirname $0`
 #export CLASSPATH=$ROOT_DIR/classes:$ROOT_DIR/lib/*:/Library/gurobi301/mac64/lib/gurobi.jar:/home/hltcoe/mgormley/working/tagging/bin/gurobi301/linux64/lib/gurobi.jar
 export CLASSPATH=$ROOT_DIR/classes:$ROOT_DIR/lib/*:/home/hltcoe/mgormley/working/tagging/bin/gurobi301/linux64/lib/gurobi.jar:/home/hltcoe/mgormley/installed/ILOG/CPLEX_Studio_AcademicResearch122/cplex/lib/cplex.jar
-export PYTHONPATH=$ROOT_DIR/scripts
+export PYTHONPATH=$ROOT_DIR/scripts:$PYTHONPATH
 export PATH=$ROOT_DIR/bin:$PATH
