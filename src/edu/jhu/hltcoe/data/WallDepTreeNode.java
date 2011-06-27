462e8a3ade947b94781e239fe5a9eb964365b47b
Matt Gormley <mrg@cs.jhu.edu>
2011-06-27 20:52:08 +0000

    Changing wall's id so that Delta Histogram works correctly.
    git-svn-id: svn+ssh://external.hltcoe.jhu.edu/home/hltcoe/mgormley/public/repos/dep_parse_filtered/trunk@131 0408881f-cb4b-4750-8b71-c94bcb194269

--- src/edu/jhu/hltcoe/data/WallDepTreeNode.java
+++ src/edu/jhu/hltcoe/data/WallDepTreeNode.java
@@ -3,7 +3,7 @@ package edu.jhu.hltcoe.data;
 
 public class WallDepTreeNode extends DepTreeNode {
 
-    public static final String WALL_ID = "$WALL$";
+    public static final String WALL_ID = "__WALL__";
     public static final Label WALL_LABEL = new TaggedWord(WALL_ID, WALL_ID);
     public static final int WALL_POSITION = -1;
     
