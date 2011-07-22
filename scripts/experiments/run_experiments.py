694848d7117f1209195d3f3345c930cb3781c5cb
Matt Gormley <mrg@cs.jhu.edu>
2011-07-22 20:26:25 +0000

    Adding virtual free to pipeline.py for all queues
    git-svn-id: svn+ssh://external.hltcoe.jhu.edu/home/hltcoe/mgormley/public/repos/dep_parse_filtered/trunk@172 0408881f-cb4b-4750-8b71-c94bcb194269

--- scripts/experiments/run_experiments.py
+++ scripts/experiments/run_experiments.py
@@ -111,7 +111,8 @@ class DepParseExpParamsRunner(ExpParamsRunner):
             
         if self.queue == "mem" or self.queue == "himem":
             # Override qsub_args to exclude "-l h_vmem=%dM"
-            self.qsub_args = re.sub("-l h_vmem=", "-l virtual_free=", self.qsub_args)
+            # Old way: self.qsub_args = re.sub("-l h_vmem=", "-l virtual_free=", self.qsub_args)
+            self.qsub_args = re.sub("-l h_vmem=\S+", "", self.qsub_args)
             
     def get_experiments(self):
         all = DPExpParams()
