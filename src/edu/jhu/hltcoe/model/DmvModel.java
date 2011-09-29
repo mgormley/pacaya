11cfd31f79a57c58c6a279a7ec45c5b69c82c18d
Matt Gormley <mrg@cs.jhu.edu>
2011-09-29 19:57:17 +0000

    Adding comments
    git-svn-id: svn+ssh://external.hltcoe.jhu.edu/home/hltcoe/mgormley/public/repos/dep_parse_filtered/trunk@181 0408881f-cb4b-4750-8b71-c94bcb194269

--- src/edu/jhu/hltcoe/model/DmvModel.java
+++ src/edu/jhu/hltcoe/model/DmvModel.java
@@ -7,10 +7,10 @@ import edu.jhu.hltcoe.data.Label;
 import edu.jhu.hltcoe.util.Triple;
 
 public class DmvModel implements Model {
-    
+
     private Map<Triple<Label, String, Boolean>, Double> swMap = new HashMap<Triple<Label, String, Boolean>, Double>();
     private Map<Triple<Label, String, Label>, Double> cwMap = new HashMap<Triple<Label, String, Label>, Double>();
-    
+
     public Map<Triple<Label, String, Boolean>, Double> getStopWeights() {
         return swMap;
     }
@@ -19,10 +19,27 @@ public class DmvModel implements Model {
         return cwMap;
     }
 
+    /**
+     * @param triple
+     *            A Triple consisting of (parent, lr, adj), where parent is the
+     *            label of the current node, lr describes whether the model is
+     *            generating to the left or right and is \in {"l", "r"}, and adj
+     *            is true iff generating an adjacent node.
+     * @param weight
+     *            The probability of stopping.
+     */
     public void putStopWeight(Triple<Label, String, Boolean> triple, double weight) {
         swMap.put(triple, weight);
     }
 
+    /**
+     * @param triple
+     *            A Triple consisting of (parent, lr, child). lr describes
+     *            whether the model is generating to the left or right and is
+     *            \in {"l", "r"}. parent and child are the labels of the nodes.
+     * @param d
+     *            The probability of generating child to the lr of parent.
+     */
     public void putChooseWeight(Triple<Label, String, Label> triple, double d) {
         cwMap.put(triple, d);
     }
