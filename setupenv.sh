4b964a15d7947edf14fd0ea6c00b9efee8849af6
Matt Gormley <mrg@cs.jhu.edu>
2012-01-11 18:19:12 +0000

    Adding dip_parse dir to PATH
    git-svn-id: svn+ssh://external.hltcoe.jhu.edu/home/hltcoe/mgormley/public/repos/dep_parse_filtered/trunk@208 0408881f-cb4b-4750-8b71-c94bcb194269

--- setupenv.sh
+++ setupenv.sh
@@ -3,4 +3,4 @@ ROOT_DIR=`pwd`
 #export CLASSPATH=$ROOT_DIR/classes:$ROOT_DIR/lib/*:/Library/gurobi301/mac64/lib/gurobi.jar:/home/hltcoe/mgormley/working/tagging/bin/gurobi301/linux64/lib/gurobi.jar
 export CLASSPATH=$ROOT_DIR/classes:$ROOT_DIR/lib/*:/home/hltcoe/mgormley/working/tagging/bin/gurobi301/linux64/lib/gurobi.jar:/home/hltcoe/mgormley/installed/ILOG/CPLEX_Studio_AcademicResearch122/cplex/lib/cplex.jar
 export PYTHONPATH=$ROOT_DIR/scripts:$PYTHONPATH
-export PATH=$ROOT_DIR/bin:$PATH
+export PATH=$ROOT_DIR/bin:$ROOT_DIR/dip_parse:$PATH
