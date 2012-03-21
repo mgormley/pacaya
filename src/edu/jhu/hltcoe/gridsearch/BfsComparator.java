4cf84d08c7cdf57b581ef829858c9d7364ce99ab
Matt Gormley <mrg@cs.jhu.edu>
2012-03-21 17:59:35 +0000

    Adding best first search comparator
    git-svn-id: svn+ssh://external.hltcoe.jhu.edu/home/hltcoe/mgormley/public/repos/dep_parse_filtered/trunk@250 0408881f-cb4b-4750-8b71-c94bcb194269

---
+++ src/edu/jhu/hltcoe/gridsearch/BfsComparator.java
@@ -0,0 +1,30 @@
+package edu.jhu.hltcoe.gridsearch;
+
+import java.util.Comparator;
+
+/**
+ * This compartor will sort the elements so that a best first search is
+ * performed
+ */
+public class BfsComparator implements Comparator<ProblemNode> {
+
+    @Override
+    /** 
+     * Head in the PQ will be the least element. In this case, 
+     * it is the one with the highest (since this does maximization) 
+     * optimistic bound.
+     */
+    public int compare(ProblemNode node1, ProblemNode node2) {
+        // a negative integer, zero, or a positive integer as the first
+        // argument is less than, equal to, or greater than the second.
+
+        // If the depths are equal, compare the optimistic bounds
+        if (node1.getOptimisticBound() > node2.getOptimisticBound()) {
+            return -1;
+        } else if (node1.getOptimisticBound() < node2.getOptimisticBound()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
