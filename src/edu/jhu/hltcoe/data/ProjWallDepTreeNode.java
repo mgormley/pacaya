3c5fb344285927f388566722b081569a0843ac7a
Matt Gormley <mrg@cs.jhu.edu>
2012-03-27 18:36:11 +0000

    Removing duplicate constants
    git-svn-id: svn+ssh://external.hltcoe.jhu.edu/home/hltcoe/mgormley/public/repos/dep_parse_filtered/trunk@265 0408881f-cb4b-4750-8b71-c94bcb194269

--- src/edu/jhu/hltcoe/data/ProjWallDepTreeNode.java
+++ src/edu/jhu/hltcoe/data/ProjWallDepTreeNode.java
@@ -2,14 +2,10 @@ package edu.jhu.hltcoe.data;
 
 
 public class ProjWallDepTreeNode extends ProjDepTreeNode {
-
-    public static final String WALL_ID = "__WALL__";
-    public static final Label WALL_LABEL = new TaggedWord(WALL_ID, WALL_ID);
-    public static final int WALL_POSITION = -1;
     
     public ProjWallDepTreeNode() {
-        super(WALL_LABEL);
-        setPosition(WALL_POSITION);
+        super(WallDepTreeNode.WALL_LABEL);
+        setPosition(WallDepTreeNode.WALL_POSITION);
     }
 
     @Override
