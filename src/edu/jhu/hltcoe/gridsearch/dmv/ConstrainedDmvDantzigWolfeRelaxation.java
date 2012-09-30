c7744df4528d7afc0732719eefc412d7ea776f04
Matt Gormley <mrg@cs.jhu.edu>
2012-09-30 01:19:08 +0000

    Committing some stubs of constrained classes.
    git-svn-id: svn+ssh://external.hltcoe.jhu.edu/home/hltcoe/mgormley/public/repos/dep_parse_filtered/trunk@454 0408881f-cb4b-4750-8b71-c94bcb194269

---
+++ src/edu/jhu/hltcoe/gridsearch/dmv/ConstrainedDmvDantzigWolfeRelaxation.java
@@ -0,0 +1,24 @@
+package edu.jhu.hltcoe.gridsearch.dmv;
+
+import ilog.concert.IloRange;
+
+import java.io.File;
+
+import edu.jhu.hltcoe.gridsearch.dmv.DmvDantzigWolfeRelaxation.MasterProblem;
+
+// TODO: finish writing this class.
+public class ConstrainedDmvDantzigWolfeRelaxation extends DmvDantzigWolfeRelaxation {
+
+    protected static class ConstrainedMasterProblem extends MasterProblem {
+        public IloRange couplConsShinyEdges;
+    }
+    
+    public ConstrainedDmvDantzigWolfeRelaxation(File tempDir, int maxCutRounds, CutCountComputer initCutCountComp) {
+        super(tempDir, maxCutRounds, initCutCountComp);
+    }
+
+    protected MasterProblem getNewMasterProblem() {
+        return new ConstrainedMasterProblem();
+    }
+
+}
