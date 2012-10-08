cd283bbabbeeacf664cd4763620583b8130d1d45
Matt Gormley <mrg@cs.jhu.edu>
2012-10-08 15:41:01 +0000

    Reducing use of deprecated methods.
    git-svn-id: svn+ssh://external.hltcoe.jhu.edu/home/hltcoe/mgormley/public/repos/dep_parse_filtered/trunk@464 0408881f-cb4b-4750-8b71-c94bcb194269

--- src/edu/jhu/hltcoe/model/dmv/SimpleStaticDmvModel.java
+++ src/edu/jhu/hltcoe/model/dmv/SimpleStaticDmvModel.java
@@ -3,7 +3,7 @@ package edu.jhu.hltcoe.model.dmv;
 import util.Alphabet;
 import edu.jhu.hltcoe.data.Label;
 import edu.jhu.hltcoe.data.TaggedWord;
-import edu.jhu.hltcoe.data.WallDepTreeNode;
+import edu.jhu.hltcoe.model.dmv.DmvModel.Lr;
 
 public class SimpleStaticDmvModel {
 
@@ -11,7 +11,7 @@ public class SimpleStaticDmvModel {
     public static final TaggedWord TW_B = new TaggedWord("b", "B");
 
     public static DmvModel getTwoPosTagInstance() {
-        DmvModelFactory modelFactory = new RandomDmvModelFactory(0.1);
+        DmvModelFactory modelFactory = new UniformDmvModelFactory();
         Alphabet<Label> alphabet = new Alphabet<Label>();
         alphabet.lookupObject(TW_A);
         alphabet.lookupObject(TW_B);
@@ -20,18 +20,18 @@ public class SimpleStaticDmvModel {
         dmvModel.fill(0.0);
         dmvModel.putRootWeight(TW_A, 0.5);
         dmvModel.putRootWeight(TW_B, 0.5);
-        dmvModel.putChildWeight(TW_A, "l", TW_A, 0.5);
-        dmvModel.putChildWeight(TW_A, "l", TW_B, 0.5);
-        dmvModel.putChildWeight(TW_A, "r", TW_A, 1.0); // dummy param
-        dmvModel.putChildWeight(TW_B, "l", TW_A, 0.5);
-        dmvModel.putChildWeight(TW_B, "l", TW_B, 0.5);
-        dmvModel.putChildWeight(TW_B, "r", TW_B, 1.0);
+        dmvModel.putChildWeight(TW_A, Lr.LEFT, TW_A, 0.5);
+        dmvModel.putChildWeight(TW_A, Lr.LEFT, TW_B, 0.5);
+        dmvModel.putChildWeight(TW_A, Lr.RIGHT, TW_A, 1.0); // dummy param
+        dmvModel.putChildWeight(TW_B, Lr.LEFT, TW_A, 0.5);
+        dmvModel.putChildWeight(TW_B, Lr.LEFT, TW_B, 0.5);
+        dmvModel.putChildWeight(TW_B, Lr.RIGHT, TW_B, 1.0);
         
         dmvModel.fillStopProbs(1.0);
-        dmvModel.putStopProb(TW_A, "l", true, 0.6);
-        dmvModel.putStopProb(TW_A, "r", true, 0.6); 
-        dmvModel.putStopProb(TW_B, "l", true, 0.6); 
-        dmvModel.putStopProb(TW_B, "r", true, 0.6);
+        dmvModel.putStopProb(TW_A, Lr.LEFT, true, 0.6);
+        dmvModel.putStopProb(TW_A, Lr.RIGHT, true, 0.6); 
+        dmvModel.putStopProb(TW_B, Lr.LEFT, true, 0.6); 
+        dmvModel.putStopProb(TW_B, Lr.RIGHT, true, 0.6);
         
         dmvModel.convertRealToLog();
         dmvModel.assertNormalized(1e-13);
@@ -43,7 +43,7 @@ public class SimpleStaticDmvModel {
     public static final TaggedWord verb = new TaggedWord("Verb", "V");
     
     public static DmvModel getThreePosTagInstance() {
-        DmvModelFactory modelFactory = new RandomDmvModelFactory(0.1);
+        DmvModelFactory modelFactory = new UniformDmvModelFactory();
         Alphabet<Label> alphabet = new Alphabet<Label>();
         alphabet.lookupObject(noun);
         alphabet.lookupObject(adj);
@@ -52,20 +52,20 @@ public class SimpleStaticDmvModel {
 
         dmvModel.fill(0.0);
         dmvModel.putRootWeight(verb, 1.0);
-        dmvModel.putChildWeight(noun, "l", adj, 1.0);
-        dmvModel.putChildWeight(noun, "r", verb, 1.0);
-        dmvModel.putChildWeight(adj, "l", adj, 1.0);
-        dmvModel.putChildWeight(adj, "r", adj, 1.0);
-        dmvModel.putChildWeight(verb, "l", noun, 1.0);
-        dmvModel.putChildWeight(verb, "r", noun, 1.0);
+        dmvModel.putChildWeight(noun, Lr.LEFT, adj, 1.0);
+        dmvModel.putChildWeight(noun, Lr.RIGHT, verb, 1.0);
+        dmvModel.putChildWeight(adj, Lr.LEFT, adj, 1.0);
+        dmvModel.putChildWeight(adj, Lr.RIGHT, adj, 1.0);
+        dmvModel.putChildWeight(verb, Lr.LEFT, noun, 1.0);
+        dmvModel.putChildWeight(verb, Lr.RIGHT, noun, 1.0);
         
         dmvModel.fillStopProbs(1.0);
-        dmvModel.putStopProb(noun, "l", true, 0.6);
-        //dmvModel.putStopWeight(noun, "l", false, 0.8);
-        dmvModel.putStopProb(noun, "r", true, 0.9); // Allow elephant saw cat that saw mouse
-        dmvModel.putStopProb(adj, "l", true, 0.8);
-        dmvModel.putStopProb(verb, "l", true, 0.0); // Always have a subject and object
-        dmvModel.putStopProb(verb, "r", true, 0.0);
+        dmvModel.putStopProb(noun, Lr.LEFT, true, 0.6);
+        //dmvModel.putStopWeight(noun, Lr.LEFT, false, 0.8);
+        dmvModel.putStopProb(noun, Lr.RIGHT, true, 0.9); // Allow elephant saw cat that saw mouse
+        dmvModel.putStopProb(adj, Lr.LEFT, true, 0.8);
+        dmvModel.putStopProb(verb, Lr.LEFT, true, 0.0); // Always have a subject and object
+        dmvModel.putStopProb(verb, Lr.RIGHT, true, 0.0);
         
         dmvModel.convertRealToLog();
         dmvModel.assertNormalized(1e-13);
