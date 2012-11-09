81420afda77bcfc7c733d73d7c863d453f147840
Matt Gormley <mrg@cs.jhu.edu>
2012-11-09 16:00:19 +0000

    moving myplot
    git-svn-id: svn+ssh://external.hltcoe.jhu.edu/home/hltcoe/mgormley/public/repos/dep_parse_filtered/trunk@514 0408881f-cb4b-4750-8b71-c94bcb194269

--- scripts/plot/bnb-semi.R
+++ scripts/plot/bnb-semi.R
@@ -7,6 +7,12 @@ stdwidth <- 4.7
 stdheight <- 3.4
 quartz(width=stdwidth,height=stdheight)
 
+myplot <- function(p, filename) {
+  p <- p + theme_bw(12, base_family="serif")
+  print(p)
+  ggsave(filename, width=stdwidth, height=stdheight)
+}
+
 ## Read data
 results.file = "/Users/mgormley/Documents/JHU4_S10/dep_parse/results/bnb_semi/bnb_016.data"
 
@@ -21,12 +27,6 @@ groups <- daply(df, .(groupLevel), function(dat) { return(dat) })
 getDataset <- function(mydata) {
   colsubset <- df[,c("dataset", "maxNumSentences", "maxSentenceLength")]
   unique(str_c(df$dataset, df$maxNumSentences, df$maxSentenceLength, sep="."))
-}
-
-myplot <- function(p, filename) {
-  p <- p + theme_bw(12, base_family="serif")
-  print(p)
-  ggsave(filename, width=stdwidth, height=stdheight)
 }
 
 plotreldiff <- function(mydata) {
@@ -185,28 +185,3 @@ plotnumtrees <- function(mydata) {
 myplot(plotnumfathom(dfBoth), str_c(results.file, "fathom-rate", "pdf", sep="."))
 
 
-
-
-## Read data
-results.file = "/Users/mgormley/Documents/JHU4_S10/dep_parse/results/viterbi-em/results.data"
-df <- read.table(results.file, header=TRUE)
-
-plotaccuracyvslikelihood <- function(mydata) {
-  title = "Penn Treebank, Brown"
-  ##title = str_c(getDataset(mydata), unique(df$offsetProb), sep=".")
-  xlab = "Per token log-likelihood (train)"
-  ylab = "Accuracy (train)"
-  p <- ggplot(mydata, aes(x=trainLogLikelihood/numWords,
-                          y=trainAccuracy, color=initWeights))
-  p <- p + geom_point()
-  p <- p + xlab(xlab) + ylab(ylab) + opts(title=title)
-  p <- p + scale_color_discrete(name="Initialization")
-  p <- p + geom_smooth()
-}
-myplot(plotaccuracyvslikelihood(df),
-       str_c(results.file, "accvlike", "pdf", sep="."))
-
-library(Hmisc)
-dfCorr <- subset(df, initWeights == "random")
-dfCorr <- dfCorr[,c("trainAccuracy", "trainLogLikelihood")]
-rcorr(as.matrix(dfCorr), type="pearson")
