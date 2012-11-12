78997bd475a27455556183ca40af3c303c80cd16
Matt Gormley <mrg@cs.jhu.edu>
2012-11-12 15:47:23 +0000

    restricting printouts
    git-svn-id: svn+ssh://external.hltcoe.jhu.edu/home/hltcoe/mgormley/public/repos/dep_parse_filtered/trunk@529 0408881f-cb4b-4750-8b71-c94bcb194269

--- src/edu/jhu/hltcoe/gridsearch/LazyBranchAndBoundSolver.java
+++ src/edu/jhu/hltcoe/gridsearch/LazyBranchAndBoundSolver.java
@@ -67,7 +67,7 @@ public class LazyBranchAndBoundSolver {
 
     public SearchStatus runBranchAndBound(ProblemNode rootNode, Solution initialSolution, double initialScore) {
         // Initialize
-        if (incumbentScore != initialScore) {
+        if (incumbentSolution == null) {
             // When B&B is used as a subroutine within our large-neighborhood
             // local search approach, there's no reason to repeatedly print out
             // the same incumbent.
