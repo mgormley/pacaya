09ec3df70694740276c9f1554e4bcea1844768d0
Matt Gormley <mrg@cs.jhu.edu>
2012-12-07 15:30:42 +0000

    Adding runhist alias
    git-svn-id: svn+ssh://external.hltcoe.jhu.edu/home/hltcoe/mgormley/public/repos/dep_parse_filtered/trunk@653 0408881f-cb4b-4750-8b71-c94bcb194269

--- setupenv.sh
+++ setupenv.sh
@@ -1,3 +1,5 @@
+alias runhist='history | grep run_experiments.py'
+
 ROOT_DIR=`pwd`
 #ROOT_DIR=`dirname $0`
 #export CLASSPATH=$ROOT_DIR/classes:$ROOT_DIR/lib/*:/Library/gurobi301/mac64/lib/gurobi.jar:/home/hltcoe/mgormley/working/tagging/bin/gurobi301/linux64/lib/gurobi.jar
