ccfe902f7dbfeb26c28aafa35ba2bb4d3c6e69e9
Matt Gormley <mrg@cs.jhu.edu>
2013-02-11 17:36:42 +0000

    Updating PYTHONPATH
    git-svn-id: svn+ssh://external.hltcoe.jhu.edu/home/hltcoe/mgormley/public/repos/dep_parse_filtered/trunk@761 0408881f-cb4b-4750-8b71-c94bcb194269

--- setupenv.sh
+++ setupenv.sh
@@ -2,8 +2,12 @@ alias runhist='history | grep run_experiments.py'
 
 ROOT_DIR=`pwd`
 #ROOT_DIR=`dirname $0`
+
 #export CLASSPATH=$ROOT_DIR/classes:$ROOT_DIR/lib/*:/Library/gurobi301/mac64/lib/gurobi.jar:/home/hltcoe/mgormley/working/tagging/bin/gurobi301/linux64/lib/gurobi.jar
 export CLASSPATH=$ROOT_DIR/classes:/home/hltcoe/mgormley/working/tagging/bin/gurobi301/linux64/lib/gurobi.jar:/home/hltcoe/mgormley/installed/IBM/ILOG/CPLEX_Studio125/cplex/lib/cplex.jar:$ROOT_DIR/lib/*
+
 export PYTHONPATH=$ROOT_DIR/scripts:$PYTHONPATH:/Library/Python/2.6/site-packages/
+export PYTHONPATH=$PYTHONPATH:/home/hltcoe/mgormley/installed/gdata-2.0.14/src
+
 export PATH=$ROOT_DIR/bin:$ROOT_DIR/dip_parse:$PATH
 export PATH=$ROOT_DIR/scripts/experiments:$ROOT_DIR/scripts/experiments/core:$PATH
