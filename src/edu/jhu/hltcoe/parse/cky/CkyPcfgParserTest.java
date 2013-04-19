738e44f828471df8daf6b24188f1f2fc3a84be67
Matt Gormley <mrg@cs.jhu.edu>
2013-04-19 16:40:02 -0400

    Updating CKY parser unit test

--- src/edu/jhu/hltcoe/parse/cky/CkyPcfgParserTest.java
+++ src/edu/jhu/hltcoe/parse/cky/CkyPcfgParserTest.java
@@ -10,7 +10,7 @@ import edu.jhu.hltcoe.util.Pair;
 public class CkyPcfgParserTest {
 
     @Test
-	public void testSimpleSentence() {
+	public void testSimpleSentence1() {
 		// time flies like an arrow.
 		CnfGrammarBuilder builder = new CnfGrammarBuilder();
 		builder.loadFromResource(CnfGrammarBuilderTest.timeFliesGrammarResource);
@@ -27,6 +27,29 @@ public class CkyPcfgParserTest {
 		String goldTree = "(S (NP (N time)) (VP (V flies) (PP (IN like) (NP (DT an) (N arrow)))))";
 		String treeStr = tree.getAsPennTreebankString().replaceAll("\\s+", " ");
 		Assert.assertEquals(goldTree, treeStr);
+		Assert.assertEquals(-11.0, logProb, 1e-13);
+	}
+    
+    @Test
+	public void testSimpleSentence2() {
+		// an arrow flies like time
+		CnfGrammarBuilder builder = new CnfGrammarBuilder();
+		builder.loadFromResource(CnfGrammarBuilderTest.timeFliesGrammarResource);
+		
+		CnfGrammar grammar = builder.getGrammar();
+				
+		Pair<CfgTree, Double> pair = parseSentence("an arrow flies like time", grammar);
+		CfgTree tree = pair.get1();
+		double logProb = pair.get2();
+		
+		System.out.println(logProb);
+		System.out.println(tree);
+		System.out.println(tree.getAsPennTreebankString());
+		String treeStr = tree.getAsPennTreebankString().replaceAll("\\s+", " ");
+		//System.out.println(treeStr);
+		String goldTree = "(S (NP (DT an) (N arrow)) (VP (V flies) (PP (IN like) (NP (N time)))))";
+		Assert.assertEquals(goldTree, treeStr);
+		Assert.assertEquals(-11.0, logProb, 1e-13);
 	}
 	
 	public static Pair<CfgTree, Double> parseSentence(String sentence, CnfGrammar grammar) {
