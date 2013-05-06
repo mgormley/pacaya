d394d9b80ea2cd0ce447b6ec41ad1da0a2a6272f
Matt Gormley <mrg@cs.jhu.edu>
2013-05-06 14:14:01 -0400

    Updating plots with latest test results.

--- scripts/plot/viterbi-vs-bnb.R
+++ scripts/plot/viterbi-vs-bnb.R
@@ -107,8 +107,9 @@ df$method <- ""
 df$method <- ifelse(df$algorithm == "bnb" , str_c(df$method, "RLT"), df$method)
 df$method <- ifelse(df$envelopeOnly == "True", str_c(df$method, " Conv.Env."), df$method)
 df$method <- ifelse(df$rltFilter == "obj-var" & !(df$envelopeOnly == "True"), str_c(df$method, " Obj.Filter"), df$method)
-##df$method <- ifelse(df$rltFilter == "max", str_c(df$method, " Max ", df$rltInitMax), df$method)
+## ON FOR ACL SUBMISSION:
 df$method <- ifelse(df$rltFilter == "max", str_c(df$method, " Max.", df$rltInitMax/1000, "k"), df$method)
+##df$method <- ifelse(df$rltFilter == "max", str_c(df$method, " Max.", df$rltInitMax/1000, "k.", df$rltCutMax/1000, "k"), df$method)
 df$method <- ifelse(df$algorithm == "viterbi", "Viterbi EM", df$method)
 
 #df$method <- str_c(df$envelopeOnly, df$rltInitMax, sep=" / ")
@@ -121,9 +122,24 @@ methodDescription <- "Algorithm"
 ##df$method = str_c(df$algorithm, sep=".")
 ##df <- subset(df, algorithm == "viterbi" | algorithm == "bnb" )
 ##df <- subset(df, time/1000/60 < 61)
-df <- subset(df, is.na(rltInitMax) | rltInitMax == 1000 | rltInitMax == 10000 | rltInitMax == 100001 | !is.finite(rltInitMax))
+df <- subset(df, dataset == "wsj200")
+df <- subset(df, is.na(rltInitMax) | rltInitMax == 1000 | rltInitMax == 10000 | rltInitMax == 100000 | !is.finite(rltInitMax))
 df <- subset(df, maxNumSentences == 200)
+## ON FOR ACL SUBMISSION:
 df <- subset(df,is.na(rltCutMax) | !is.finite(rltCutMax) | rltCutMax == 0 )
+
+df$perTokenCrossEntropy <- ifelse(df$dataset == "wsj200", - df$incumbentLogLikelihood / log(2) / 1365, NULL)
+
+# Remove any solution found after 8 hours.
+df <- subset(df, time / 1000 / 60 / 60 < 8)
+# Set the 8 hour incumbent as the best one seen in under 8 hours.
+##df$time <- max(df$time, 8*60*60*1000)
+myfun <- function(x) {
+  x[which.max(x$incumbentLogLikelihood),]
+}
+ddf <- ddply(df, .(method, universalPostCons), myfun)
+ddf$time <- 8*60*60*1000
+df <- rbind(ddf, df)
 
 plotLogLikeVsTime <- function(mydata) {
   title = "Penn Treebank, Brown"
@@ -131,8 +147,9 @@ plotLogLikeVsTime <- function(mydata) {
   ylab = "Log-likelihood (train)"
   #mydata$time <- max(mydata$time, 1.0)
   mydata$minutes <- mydata$time / 1000 / 60
-  mydata$minutes <- mydata$time / 1000 
+  #mydata$minutes <- mydata$time / 1000 
   mydata$minutes[which(mydata$minutes < 1.0)] <- 1.0
+
   p <- ggplot(mydata, aes(x=minutes,
                           y=incumbentLogLikelihood, color=method))
   p <- p + geom_point(aes(shape=method))
@@ -142,7 +159,8 @@ plotLogLikeVsTime <- function(mydata) {
   p <- p + scale_color_discrete(name=methodDescription) 
   p <- p + scale_shape_discrete(name=methodDescription)
   p <- p + scale_linetype_discrete(name="Posterior Constraints")
-  p <- p + scale_x_log10()
+  #p <- p + scale_x_log10()
+  #p <- p + scale_y_log10()
 }
 
 plotAccVsTime <- function(mydata) {
@@ -159,12 +177,15 @@ plotAccVsTime <- function(mydata) {
   p <- p + scale_color_discrete(name=methodDescription)
   p <- p + scale_shape_discrete(name=methodDescription)
   p <- p + scale_linetype_discrete(name="Posterior Constraints")
-  p <- p + scale_x_log10()
+  #p <- p + scale_x_log10()
 }
 
 
 myplot(plotLogLikeVsTime(subset(df, universalPostCons == "False")),
        str_c(results.file, "llvtime", "pdf", sep="."))
+
+myplot(plotAccVsTime(subset(df, universalPostCons == "True")),
+       str_c(results.file, "accvtime", "pdf", sep="."))
 
 myplot(plotLogLikeVsTime(df),
        str_c(results.file, "llvtime", "pdf", sep="."))
