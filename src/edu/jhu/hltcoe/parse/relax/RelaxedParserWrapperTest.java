abb01cb52df0cb25b625bbeedb8ae29acd50345c
Matt Gormley <mrg@cs.jhu.edu>
2013-06-03 16:26:22 -0400

    Bug fix: test wasn't setting the prng seed.

--- src/edu/jhu/hltcoe/parse/relax/RelaxedParserWrapperTest.java
+++ src/edu/jhu/hltcoe/parse/relax/RelaxedParserWrapperTest.java
@@ -1,6 +1,7 @@
 package edu.jhu.hltcoe.parse.relax;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import edu.jhu.hltcoe.data.DepTreebank;
@@ -10,10 +11,16 @@ import edu.jhu.hltcoe.model.dmv.DmvModelFactory;
 import edu.jhu.hltcoe.model.dmv.RandomDmvModelFactory;
 import edu.jhu.hltcoe.parse.relax.RelaxedParserWrapper.RelaxedParserWrapperPrm;
 import edu.jhu.hltcoe.train.DmvTrainCorpus;
+import edu.jhu.hltcoe.util.Prng;
 
 
 public class RelaxedParserWrapperTest {
 
+    @Before
+    public void setUp() {
+        Prng.seed(Prng.DEFAULT_SEED);
+    }
+    
     @Test
     public void testProjection() {
         SentenceCollection sentences = new SentenceCollection();
