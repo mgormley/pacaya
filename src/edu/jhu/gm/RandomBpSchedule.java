0ddd93bc7f1d44f35f1d86c7edce651936031698
Matt Gormley <mrg@cs.jhu.edu>
2013-06-25 11:20:36 -0400

    Adding a randomly ordered schedule to BP

--- src/edu/jhu/gm/RandomBpSchedule.java
+++ src/edu/jhu/gm/RandomBpSchedule.java
@@ -1,15 +1,22 @@
 package edu.jhu.gm;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import edu.jhu.gm.FactorGraph.FgEdge;
 
 public class RandomBpSchedule implements BpSchedule {
 
+    private ArrayList<FgEdge> order;
+    
+    public RandomBpSchedule(FactorGraph fg) {
+        order = new ArrayList<FgEdge>(fg.getEdges());
+    }
     @Override
     public List<FgEdge> getOrder() {
-        // TODO Auto-generated method stub
-        return null;
+        Collections.shuffle(order);
+        return order;
     }
 
 }
