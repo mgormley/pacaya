c8fbd116cd4c6097f4bf481829cdac9b9921665c
Matt Gormley <mrg@cs.jhu.edu>
2013-06-27 09:49:20 -0400

    Fixing ant build.

--- setupenv.sh
+++ setupenv.sh
@@ -4,7 +4,7 @@ ROOT_DIR=`pwd`
 #ROOT_DIR=`dirname $0`
 
 #export CLASSPATH=$ROOT_DIR/classes:$ROOT_DIR/lib/*:/Library/gurobi301/mac64/lib/gurobi.jar:/home/hltcoe/mgormley/working/tagging/bin/gurobi301/linux64/lib/gurobi.jar
-export CLASSPATH=$ROOT_DIR/classes:/home/hltcoe/mgormley/working/tagging/bin/gurobi301/linux64/lib/gurobi.jar:/home/hltcoe/mgormley/installed/IBM/ILOG/CPLEX_Studio125/cplex/lib/cplex.jar:$ROOT_DIR/lib/*
+export CLASSPATH=$ROOT_DIR/target/classes:/home/hltcoe/mgormley/working/tagging/bin/gurobi301/linux64/lib/gurobi.jar:/home/hltcoe/mgormley/installed/IBM/ILOG/CPLEX_Studio125/cplex/lib/cplex.jar:$ROOT_DIR/lib/*
 
 export PYTHONPATH=$ROOT_DIR/scripts:$PYTHONPATH:/Library/Python/2.6/site-packages/
 export PYTHONPATH=$PYTHONPATH:/home/hltcoe/mgormley/installed/gdata-2.0.14/src
