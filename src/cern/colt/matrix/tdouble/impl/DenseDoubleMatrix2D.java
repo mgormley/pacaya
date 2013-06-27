e341ada1d56e063c9eb02b71e11a5330486115fc
Matt Gormley <mrg@cs.jhu.edu>
2013-06-26 22:22:35 -0400

    Adding empty colt stubs for matrix classes

---
+++ src/cern/colt/matrix/tdouble/impl/DenseDoubleMatrix2D.java
@@ -0,0 +1,58 @@
+package cern.colt.matrix.tdouble.impl;
+
+public class DenseDoubleMatrix2D {
+
+    public DenseDoubleMatrix2D(int nRows, int i) {
+        // TODO Auto-generated constructor stub
+    }
+
+    public DenseDoubleMatrix2D(double[][] avals) {
+        // TODO Auto-generated constructor stub
+    }
+
+    public void setQuick(int i, int j, double g) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public int rows() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public int columns() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public double getQuick(int i, int j) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public DenseDoubleMatrix2D viewDice() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void zMult(DenseDoubleMatrix2D b, DenseDoubleMatrix2D sb) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void set(int i, int j, double d) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public double[][] toArray() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void zMult(SparseCCDoubleMatrix2D b, DenseDoubleMatrix2D c1) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
---
+++ src/cern/colt/matrix/tdouble/impl/SparseRCDoubleMatrix2D.java
@@ -0,0 +1,9 @@
+package cern.colt.matrix.tdouble.impl;
+
+public class SparseRCDoubleMatrix2D {
+
+    public SparseRCDoubleMatrix2D(double[][] array) {
+        // TODO Auto-generated constructor stub
+    }
+
+}
