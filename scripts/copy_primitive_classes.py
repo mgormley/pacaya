542f865ddb6164996a709a068983a337c30f1a73
Matt Gormley <mrg@cs.jhu.edu>
2013-07-10 15:05:43 -0400

    Updating prims script

--- scripts/copy_primitive_classes.py
+++ scripts/copy_primitive_classes.py
@@ -32,21 +32,31 @@ def re_sub_all(re_subs, s):
         print "Number of substitutions for %s-->%s: %d" % (k, v, num_subs)
     return s
     
-def copy_pair(dest_key, dest_val, dest_key_prim, dest_val_prim):
-    repls = [("Long", dest_key),
+def copy_pair(dest_key, dest_val, dest_key_prim, dest_val_prim, dest_key_const, dest_val_const):
+    repls = []
+    # Prepend some replacements for generics in the unit tests. 
+    if dest_key == "Int":
+        repls += [("<Long,", "<Integer,")]
+    if dest_val == "Int":
+        repls += ["Double>", "Integer>"]
+    # Add the primary replacements.
+    repls += [("Long", dest_key),
              ("Double", dest_val),
              ("long", dest_key_prim),
              ("double", dest_val_prim),
+             ("LONG", dest_key_const),
+             ("DOUBLE", dest_val_const),
              ("getIntIndexArray", "getIndexArray"),
              ("int serialVersionUID", "long serialVersionUID"),
              ]
+    # Other regex replacements
     add_re_subs = [(r"SafeCast\.safeIntToInt\(([^\)]+)\)", r"\1"),
                ]
     # Convert repls to regex replacements.
     re_subs = [(re.escape(k), v) for k, v in repls]
     re_subs += add_re_subs
     assert len(re_subs) == len(repls) + len(add_re_subs)
-    
+        
     classes = ["edu.jhu.util.vector.LongDoubleMap",
                "edu.jhu.util.vector.LongDoubleEntry",
                  "edu.jhu.util.vector.SortedLongDoubleMap",
@@ -63,11 +73,10 @@ def copy_pair(dest_key, dest_val, dest_key_prim, dest_val_prim):
     for sf, df in zip(src_files, dest_files):
         print "Current destination file:", df
         dest_str = file_to_str(sf)
-        #dest_str = replace_all(repls, dest_str)
         dest_str = re_sub_all(re_subs, dest_str)
         str_to_file(dest_str, df)
     
 
 if __name__ == "__main__":
-    copy_pair("Int", "Double", "int", "double")
+    copy_pair("Int", "Double", "int", "double", "INT", "DOUBLE")
 
