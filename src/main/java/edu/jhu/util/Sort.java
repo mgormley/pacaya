ff8dabc98b920e53a993b20b534935599b9a1c39
Matt Gormley <mrg@cs.jhu.edu>
2013-07-11 16:52:45 -0400

    Adding sort for long[]/double[] pairs.

--- src/main/java/edu/jhu/util/Sort.java
+++ src/main/java/edu/jhu/util/Sort.java
@@ -16,6 +16,8 @@ public class Sort {
         // private constructor
     }
 
+    /* ------------------- Doubles only --------------- */
+    
     /**
      * Performs an in-place quick sort on array. Sorts in descending order.
      */
@@ -70,6 +72,8 @@ public class Sort {
         return Utilities.getIndexArray(values.length);
     }
 
+    /* ------------------- Ints and Doubles --------------- */        
+    
     /**
      * Performs an in-place quick sort on values. All the sorting operations on values
      * are mirrored in index. Sorts in descending order.
@@ -175,21 +179,6 @@ public class Sort {
         swap(values, array, storeIndex, right);
         return storeIndex;
     }
-    
-
-    /**
-     * Returns whether x is less than y.
-     */
-    private static boolean ltInt(int x, int y) {
-        return (x < y);
-    }
-    
-    /**
-     * Returns whether x is less than y.
-     */
-    private static boolean ltDouble(double x, double y) {
-        return (x < y);
-    }
 
     /**
      * Swaps the elements at positions i and j in both the values and index array, which must be the same length.
@@ -203,6 +192,132 @@ public class Sort {
         swap(index, i, j);
     }
 
+    public static long[] getLongIndexArray(double[] values) {
+        return Utilities.getLongIndexArray(values.length);
+    }
+    
+    /* ------------------- Longs and Doubles --------------- */
+    
+    /**
+     * Performs an in-place quick sort on values. All the sorting operations on values
+     * are mirrored in index. Sorts in descending order.
+     */
+    public static void sortValuesDesc(double[] values, long[] index) {
+        Vectors.scale(values, -1.0);
+        sortValuesAsc(values, index);
+        Vectors.scale(values, -1.0);
+    }
+    
+    /**
+     * Performs an in-place quick sort on values. All the sorting operations on values
+     * are mirrored in index. Sorts in ascending order.
+     */
+    public static void sortValuesAsc(double[] values, long[] index) {
+        quicksortValues(values, index, 0, index.length - 1);
+    }
+
+    private static void quicksortValues(double[] array, long[] index, int left, int right) {
+        if (left < right) {
+            // Choose a pivot index.
+            // --> Here we choose the rightmost element which does the least
+            // amount of work if the array is already sorted.
+            int pivotIndex = right;
+            // Partition the array so that everything less than
+            // values[pivotIndex] is on the left of pivotNewIndex and everything
+            // greater than or equal is on the right.
+            int pivotNewIndex = partitionValues(array, index, left, right, pivotIndex);
+            // Recurse on the left and right sides.
+            quicksortValues(array, index, left, pivotNewIndex - 1);
+            quicksortValues(array, index, pivotNewIndex + 1, right);
+        }
+    }
+    
+    private static int partitionValues(double[] array, long[] index, int left, int right, int pivotIndex) {
+        double pivotValue = array[pivotIndex];
+        // Move the pivot value to the rightmost position.
+        swap(array, index, pivotIndex, right);
+        // For each position between left and right, swap all the values less
+        // than or equal to the pivot value to the left side.
+        int storeIndex = left;
+        for (int i=left; i<right; i++) {
+            if (array[i] <= pivotValue) {
+                swap(array, index, i, storeIndex);
+                storeIndex++;
+            }
+        }
+        // Move the pivot value back to the split point.
+        swap(array, index, storeIndex, right);
+        return storeIndex;
+    }
+
+    /**
+     * Performs an in-place quick sort on index. All the sorting operations on index
+     * are mirrored in values. Sorts in descending order.
+     */
+    public static void sortIndexDesc(long[] index, double[] values) {
+        Vectors.scale(index, -1);
+        sortIndexAsc(index, values);
+        Vectors.scale(index, -1);
+    }
+    
+    /**
+     * Performs an in-place quick sort on index. All the sorting operations on index
+     * are mirrored in values. Sorts in ascending order.
+     * @return index - sorted.
+     */
+    public static long[] sortIndexAsc(long[] index, double[] values) {
+        quicksortIndex(index, values, 0, index.length - 1);
+        return index;
+    }
+
+    private static void quicksortIndex(long[] array, double[] values, int left, int right) {
+        if (left < right) {
+            // Choose a pivot index.
+            // --> Here we choose the rightmost element which does the least
+            // amount of work if the array is already sorted.
+            int pivotIndex = right;
+            // Partition the array so that everything less than
+            // values[pivotIndex] is on the left of pivotNewIndex and everything
+            // greater than or equal is on the right.
+            int pivotNewIndex = partitionIndex(array, values, left, right, pivotIndex);
+            // Recurse on the left and right sides.
+            quicksortIndex(array, values, left, pivotNewIndex - 1);
+            quicksortIndex(array, values, pivotNewIndex + 1, right);
+        }
+    }
+    
+    private static int partitionIndex(long[] array, double[] values, int left, int right, int pivotIndex) {
+        long pivotValue = array[pivotIndex];
+        // Move the pivot value to the rightmost position.
+        swap(values, array, pivotIndex, right);
+        // For each position between left and right, swap all the values less
+        // than or equal to the pivot value to the left side.
+        int storeIndex = left;
+        for (int i=left; i<right; i++) {
+            if (array[i] <= pivotValue) {
+                swap(values, array, i, storeIndex);
+                storeIndex++;
+            }
+        }
+        // Move the pivot value back to the split point.
+        swap(values, array, storeIndex, right);
+        return storeIndex;
+    }
+        
+    /**
+     * Swaps the elements at positions i and j in both the values and index array, which must be the same length.
+     * @param values An array of values.
+     * @param index An array of indices.
+     * @param i The position of the first element to swap.
+     * @param j The position of the second element to swap.
+     */
+    private static void swap(double[] values, long[] index, int i, int j) {
+        swap(values, i, j);
+        swap(index, i, j);
+    }
+    
+    /* ----------------------------------------------------- */
+
     /**
      * Swaps the elements at positions i and j.
      */
@@ -211,7 +326,7 @@ public class Sort {
         array[i] = array[j];
         array[j] = valAtI;
     }
-
+    
     /**
      * Swaps the elements at positions i and j.
      */
@@ -219,6 +334,29 @@ public class Sort {
         int valAtI = array[i];
         array[i] = array[j];
         array[j] = valAtI;
+    }
+
+    /**
+     * Swaps the elements at positions i and j.
+     */
+    private static void swap(long[] array, int i, int j) {
+        long valAtI = array[i];
+        array[i] = array[j];
+        array[j] = valAtI;
+    }
+    
+    /**
+     * Returns whether x is less than y.
+     */
+    private static boolean ltInt(int x, int y) {
+        return (x < y);
+    }
+    
+    /**
+     * Returns whether x is less than y.
+     */
+    private static boolean ltDouble(double x, double y) {
+        return (x < y);
     }
 
     public static boolean isSortedAsc(int[] array) {
--- src/main/java/edu/jhu/util/SortTest.java
+++ src/main/java/edu/jhu/util/SortTest.java
@@ -12,6 +12,8 @@ import edu.jhu.util.collections.PIntArrayList;
 
 public class SortTest {
     
+    /* ---------- Doubles only --------------*/
+    
     @Test
     public void testQuicksortAsc() {
         double[] values = new double[]{ 1.0f, 3.0f, 2.0f, -1.0, 5.0};
@@ -49,6 +51,8 @@ public class SortTest {
             assertTrue(Sort.isSortedDesc(values));
         }
     }
+    
+    /* ---------- Ints and Doubles --------------*/
     
     @Test
     public void testSortValuesAsc() {
@@ -120,6 +124,80 @@ public class SortTest {
         
         JUnitUtils.assertArrayEquals(new double[]{ -1.0, 2.0, 3.0, 5.0, 1.0 }, values, 1e-13);
         Assert.assertArrayEquals(new int[]{ 8, 5, 4, 3, 1 }, index);
+    }
+    
+    /* ---------- Longs and Doubles --------------*/
+    
+    @Test
+    public void testLongDoubleSortValuesAsc() {
+        double[] values = new double[]{ 1.0f, 3.0f, 2.0f, -1.0, 5.0};
+        long[] index = Sort.getLongIndexArray(values);
+        Sort.sortValuesAsc(values, index);
+        System.out.println(Arrays.toString(values));
+        System.out.println(Arrays.toString(index));
+        
+        JUnitUtils.assertArrayEquals(new double[]{ -1.0, 1.0f, 2.0, 3.0f, 5.0}, values, 1e-13);
+        Assert.assertArrayEquals(new long[]{ 3, 0, 2, 1, 4}, index);
+    }
+    
+    @Test
+    public void testLongDoubleSortValuesDesc() {
+        double[] values = new double[]{ 1.0f, 3.0f, 2.0f, -1.0, 5.0};
+        long[] index = Sort.getLongIndexArray(values);
+        Sort.sortValuesDesc(values, index);
+        System.out.println(Arrays.toString(values));
+        System.out.println(Arrays.toString(index));
+        
+        JUnitUtils.assertArrayEquals(new double[]{ 5.0, 3.0, 2.0, 1.0, -1.0}, values, 1e-13);
+        Assert.assertArrayEquals(new long[]{ 4, 1, 2, 0, 3}, index);
+    }
+    
+    @Test
+    public void testLongDoubleSortValuesInfinitiesAsc() {
+        double[] values = new double[]{ 1.0f, Double.POSITIVE_INFINITY, 2.0f, -1.0, Double.NEGATIVE_INFINITY, 5.0};
+        long[] index = Sort.getLongIndexArray(values);
+        Sort.sortValuesAsc(values, index);
+        System.out.println(Arrays.toString(values));
+        System.out.println(Arrays.toString(index));
+
+        JUnitUtils.assertArrayEquals(new double[]{Double.NEGATIVE_INFINITY, -1.0, 1.0, 2.0, 5.0, Double.POSITIVE_INFINITY}, values, 1e-13);
+        Assert.assertArrayEquals(new long[]{ 4, 3, 0, 2, 5, 1 }, index);
+    }
+    
+    @Test
+    public void testLongDoubleSortValuesInfinitiesDesc() {
+        double[] values = new double[]{ 1.0f, Double.POSITIVE_INFINITY, 2.0f, -1.0, Double.NEGATIVE_INFINITY, 5.0};
+        long[] index = Sort.getLongIndexArray(values);
+        Sort.sortValuesDesc(values, index);
+        System.out.println(Arrays.toString(values));
+        System.out.println(Arrays.toString(index));
+        
+        JUnitUtils.assertArrayEquals(new double[]{Double.POSITIVE_INFINITY,  5.0, 2.0, 1.0, -1.0, Double.NEGATIVE_INFINITY}, values, 1e-13);
+        Assert.assertArrayEquals(new long[]{ 1, 5, 2, 0, 3, 4 }, index);
+    }    
+
+    @Test
+    public void testLongDoubleSortIndexAsc() {
+        double[] values = new double[]{ 1.0f, 3.0f, 2.0f, -1.0, 5.0};
+        long[] index = new long[] { 1, 4, 5, 8, 3};
+        Sort.sortIndexAsc(index, values);
+        System.out.println(Arrays.toString(values));
+        System.out.println(Arrays.toString(index));
+        
+        JUnitUtils.assertArrayEquals(new double[]{ 1.0, 5.0, 3.0, 2.0, -1.0 }, values, 1e-13);
+        Assert.assertArrayEquals(new long[]{ 1, 3, 4, 5, 8 }, index);
+    }
+
+    @Test
+    public void testLongDoubleSortIndexDesc() {
+        double[] values = new double[]{ 1.0f, 3.0f, 2.0f, -1.0, 5.0};
+        long[] index = new long[] { 1, 4, 5, 8, 3};
+        Sort.sortIndexDesc(index, values);
+        System.out.println(Arrays.toString(values));
+        System.out.println(Arrays.toString(index));
+        
+        JUnitUtils.assertArrayEquals(new double[]{ -1.0, 2.0, 3.0, 5.0, 1.0 }, values, 1e-13);
+        Assert.assertArrayEquals(new long[]{ 8, 5, 4, 3, 1 }, index);
     }
     
     @Test
