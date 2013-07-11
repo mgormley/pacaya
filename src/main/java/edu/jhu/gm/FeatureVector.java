f88033d609450c004f209d00d4afedd4e76a6660
Matt Gormley <mrg@cs.jhu.edu>
2013-07-11 17:04:25 -0400

    Adding additional constructors for FeatureVector.

--- src/main/java/edu/jhu/gm/FeatureVector.java
+++ src/main/java/edu/jhu/gm/FeatureVector.java
@@ -4,16 +4,26 @@ import edu.jhu.util.vector.SortedIntDoubleVector;
 
 public class FeatureVector extends SortedIntDoubleVector {
 
+    /**
+     * Feature Vectors should almost always be constructed via a
+     * FeatureVectorBuilder, which has much faster support for add() and set().
+     */
     public FeatureVector() {
         super();
     }
     
+    /** Copy constructor. */
     public FeatureVector(FeatureVector featureVector) {
         super(featureVector);
     }
 
+    /** Constructs a feature vector from a dense vector represented as an array. */
     public FeatureVector(double[] feats) {
         super(feats);
+    }
+
+    public FeatureVector(int[] index, double[] values) {
+        super(index, values);
     }
 
 }
