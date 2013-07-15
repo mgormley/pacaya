07572ce606078d15abbdfc4e3b07d91643076d2f
Meg Mitchell <margarmitchell@gmail.com>
2013-07-15 14:58:20 -0400

    Small change.

--- src/main/java/edu/jhu/featurize/GetFeatures.java
+++ src/main/java/edu/jhu/featurize/GetFeatures.java
@@ -319,7 +319,11 @@ public class GetFeatures {
             Iterator it = stringMap.entrySet().iterator();
             while (it.hasNext()) {
                 Map.Entry pairs = (Map.Entry)it.next();
+                System.out.println(pairs.getKey());
+                System.out.println(pairs.getValue());
+                System.out.println(feat);
                 feat = feat.replaceAll((String) pairs.getKey(), (String) pairs.getValue());
+                System.out.println(feat);
             }
             bw.write(feat);
             bw.newLine();
