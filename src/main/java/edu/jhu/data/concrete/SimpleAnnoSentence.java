ff22b554f0bb81e4a9498da983d977af485ee538
Matt Gormley <mrg@cs.jhu.edu>
2013-07-17 12:11:38 -0400

    Adding convenience methods to SimpleAnnoSentence for accessing words/lemmas/tags.

--- src/main/java/edu/jhu/data/concrete/SimpleAnnoSentence.java
+++ src/main/java/edu/jhu/data/concrete/SimpleAnnoSentence.java
@@ -1,6 +1,9 @@
 package edu.jhu.data.concrete;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import edu.jhu.data.Span;
 
 /**
  * Simple representation of a single sentence with many annotations.
@@ -11,6 +14,9 @@ import java.util.List;
  */
 public class SimpleAnnoSentence {
 
+    // TODO: maybe change this to something like underscore?
+    private static final String SPAN_STR_SEP = " ";
+    
     private List<String> words;
     private List<String> lemmas;
     private List<String> posTags;
@@ -28,6 +34,126 @@ public class SimpleAnnoSentence {
         
     }
 
+
+    /** Gets the i'th word as a String. */
+    public String getWord(int i) {
+        return words.get(i);
+    }
+    
+    /** Gets the i'th POS tag as a String. */
+    public String getPosTag(int i) {
+        return posTags.get(i);
+    }
+    
+    /** Gets the i'th lemma as a String. */
+    public String getLemma(int i) {
+        return lemmas.get(i);
+    }
+    
+    /**
+     * Gets a list of words corresponding to a token span.
+     */
+    public List<String> getWords(Span span) {
+        return getSpan(words, span);
+    }
+
+    /**
+     * Gets a list of POS tags corresponding to a token span.
+     */
+    public List<String> getPosTags(Span span) {
+        return getSpan(posTags, span);
+    }
+
+    /**
+     * Gets a list of lemmas corresponding to a token span.
+     */
+    public List<String> getLemmas(Span span) {
+        return getSpan(lemmas, span);
+    }
+
+    /**
+     * Gets a list of word/POS tags corresponding to a token span.
+     */
+    public List<String> getWordPosTags(Span span) {
+        assert (span != null);
+        List<String> list = new ArrayList<String>();
+        for (int i = span.start(); i < span.end(); i++) {
+            list.add(words.get(i) + "/" + posTags.get(i));
+        }
+        return list;
+    }
+    
+    /**
+     * Gets a single string representing the words in a given token span.
+     * 
+     * @param span
+     */
+    public String getWordsStr(Span span) {
+        return getSpanStr(words, span);
+    }
+
+    /**
+     * Gets a single string representing the POS tags in a given token span.
+     * 
+     * @param span
+     */
+    public String getPosTagsStr(Span span) {
+        return getSpanStr(posTags, span);
+    }
+
+    /**
+     * Gets a single string representing the lemmas in a given token span.
+     * 
+     * @param span
+     */
+    public String getLemmasStr(Span span) {
+        return getSpanStr(lemmas, span);
+    }
+
+    /**
+     * Gets a single string representing the Word/POS in a given token span.
+     * 
+     * @param span
+     */
+    public String getWordPosTagsStr(Span span) {
+        assert (span != null);
+        StringBuilder sb = new StringBuilder();
+        for (int i = span.start(); i < span.end(); i++) {
+            if (i > span.start()) {
+                sb.append(SPAN_STR_SEP);
+            }
+            sb.append(words.get(i));
+            sb.append("/");
+            sb.append(posTags.get(i));
+        }
+        return sb.toString();
+    }
+    
+    // TODO: Consider moving this to LabelSequence.
+    private static List<String> getSpan(List<String> seq, Span span) {
+        assert (span != null);
+        List<String> list = new ArrayList<String>();
+        for (int i = span.start(); i < span.end(); i++) {
+            list.add(seq.get(i));
+        }
+        return list;
+    }
+
+    // TODO: Consider moving this to LabelSequence.
+    private static String getSpanStr(List<String> seq, Span span) {
+        assert (span != null);
+        StringBuilder sb = new StringBuilder();
+        for (int i = span.start(); i < span.end(); i++) {
+            if (i > span.start()) {
+                sb.append(SPAN_STR_SEP);
+            }
+            sb.append(seq.get(i));
+        }
+        return sb.toString();
+    }
+    
+    /* ----------- Getters/Setters for internal storage ------------ */
+        
     public List<String> getWords() {
         return words;
     }
