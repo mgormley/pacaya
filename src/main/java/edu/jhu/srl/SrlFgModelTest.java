6f9816b5e50654f88a3db16287e79c53d53677f2
Matt Gormley <mrg@cs.jhu.edu>
2013-08-21 16:43:14 -0400

    Fixing serializability of SrlFgModel.

--- src/main/java/edu/jhu/srl/SrlFgModelTest.java
+++ src/main/java/edu/jhu/srl/SrlFgModelTest.java
@@ -4,10 +4,15 @@ import static org.junit.Assert.fail;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectOutputStream;
+import java.util.List;
 
 import org.junit.Test;
 
+import edu.jhu.data.conll.CoNLL09FileReader;
+import edu.jhu.data.conll.CoNLL09ReadWriteTest;
+import edu.jhu.data.conll.CoNLL09Sentence;
 import edu.jhu.featurize.SentFeatureExtractor.SentFeatureExtractorPrm;
 import edu.jhu.gm.Feature;
 import edu.jhu.gm.FgModelTest;
@@ -19,9 +24,13 @@ public class SrlFgModelTest {
     @Test
     public void testIsSerializable() throws IOException {
         try {
+            InputStream inputStream = this.getClass().getResourceAsStream(CoNLL09ReadWriteTest.conll2009Example);
+            CoNLL09FileReader cr = new CoNLL09FileReader(inputStream);
+            List<CoNLL09Sentence> sents = cr.readSents(1);
+            
             CorpusStatisticsPrm csPrm = new CorpusStatisticsPrm();
             CorpusStatistics cs = new CorpusStatistics(csPrm);
-            // TODO: cs.init(sents);
+            cs.init(sents);
             
             // Just test that no exception is thrown.
             SrlFgModel model = new SrlFgModel(FgModelTest.getFtl(), cs);
