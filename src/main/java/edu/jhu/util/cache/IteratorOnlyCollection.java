c42acf7c26e9a75813091b5491d04bee67cec506
Matt Gormley <mrg@cs.jhu.edu>
2013-11-08 11:41:55 -0500

    Adding srl-benchmark test and a GzipMap (which is slow)

---
+++ src/main/java/edu/jhu/util/cache/IteratorOnlyCollection.java
@@ -0,0 +1,31 @@
+package edu.jhu.util.cache;
+
+import java.util.AbstractSequentialList;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+/** A collection which only supports iteration. */    
+public class IteratorOnlyCollection<T> extends AbstractSequentialList<T> {
+
+    private Iterator<T> iterator;
+            
+    public IteratorOnlyCollection(Iterator<T> iterator) {
+        this.iterator = iterator;
+    }
+    
+    @Override
+    public Iterator<T> iterator() {
+        return iterator;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException();
+    }
+    
+}
