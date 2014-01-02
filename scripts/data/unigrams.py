6574b0cae792f18ab61d58d47ffd9cc8e52fbe5d
Matt Gormley <mrg@cs.jhu.edu>
2014-01-01 21:38:44 -0500

    Updates to unigrams

--- scripts/data/unigrams.py
+++ scripts/data/unigrams.py
@@ -12,23 +12,23 @@ import glob
 import time
 import gzip
 
-def read_word_counts(input):
-    total_size = os.path.getsize(input)
+def read_word_counts(in_text):
+    total_size = os.path.getsize(in_text)
     read_size = 0
     
-    if input.endswith(".gz"):
-        zf = gzip.open(input, 'rb')
+    if in_text.endswith(".gz"):
+        zf = gzip.open(in_text, 'rb')
         reader = codecs.getreader("utf-8")
         text_in = reader( zf )
     else:
-        text_in = codecs.open(input, 'r', 'utf-8')
+        text_in = codecs.open(in_text, 'r', 'utf-8')
     
     word_count_map = {}
     start0 = time.clock()
     start = time.clock()
     for line in text_in:
         #read_size += len(line.encode('utf-8'))
-        if input.endswith(".gz"):
+        if in_text.endswith(".gz"):
             # Hack fileobj from: http://stackoverflow.com/questions/1704458/get-uncompressed-size-of-a-gz-file-in-python
             read_size = zf.fileobj.tell()
         else:
@@ -42,8 +42,8 @@ def read_word_counts(input):
         elapsed = time.clock() - start
         if elapsed > 1:
             percent_read = 1.0 * read_size / total_size
-            remain = (time.clock() - start0) * 1.0 / percent_read * (1 - percent_read) / 3600.0
-            print "Percent read: %f Estimated remaining (hr): %f\n" % (percent_read, remain),
+            remain = (time.clock() - start0) * 1.0 / percent_read * (1 - percent_read) / 60.0
+            print "Percent read: %f Estimated remaining (min): %f\n" % (percent_read, remain),
             start = time.clock()
     print ""
 
@@ -87,41 +87,58 @@ def print_freq_hist(count_map, max_count=5):
     return cum_freq_hist
 
 def unigrams(options):
-    input = options.input
-    output = options.output
+    in_text = options.in_text
+    out_counts = options.out_counts
+    out_words = options.out_words
     term_limit = options.term_limit
     print "Options: ", options
     min_occur = options.min_occur
     
-    print "Counting unigrams in %s" % (input)
-    count_map = read_word_counts(input)    
+    print "Counting unigrams in %s" % (in_text)
+    count_map = read_word_counts(in_text)    
     print "Map size: %d" % (len(count_map))
     
-    print_freq_hist(count_map, 5)
-    
+    cum_freq_hist = print_freq_hist(count_map, 10)
+    if min_occur == -1 and term_limit > 0:        
+        for count in sorted(cum_freq_hist.keys()):
+            if cum_freq_hist[count] >= term_limit:
+                min_occur = count
+        print "Chose min_occur=%d to retain %d types" % (min_occur, cum_freq_hist[min_occur])
+        
     print "Converting map to list"
     hist = [(freq,term) for term,freq in count_map.items() if freq >= min_occur]
-    print "Sorted list of length: %d" % (len(hist))
+    print "Sorting list of length: %d" % (len(hist))
     hist = sorted(hist, reverse=True)
     if term_limit > 0:
+        print "Slicing to list of only %d types" % (term_limit)
         hist = hist[:term_limit]
     
-    uni_out = codecs.open(output, 'w', 'utf-8')
-    for freq, term in hist:
-        uni_out.write(str(freq))
-        uni_out.write(" ")
-        uni_out.write(term)
-        uni_out.write("\n")        
-    uni_out.close()
-    
+    if out_counts:
+        print "Writing counts to %s" % (out_counts)
+        uni_out = codecs.open(out_counts, 'w', 'utf-8')
+        for freq, term in hist:
+            uni_out.write(str(freq))
+            uni_out.write(" ")
+            uni_out.write(term)
+            uni_out.write("\n")        
+        uni_out.close()
+            
+    if out_words:
+        print "Writing words only to %s" % (out_words)
+        uni_out = codecs.open(out_words, 'w', 'utf-8')
+        for _, term in hist:
+            uni_out.write(term)
+            uni_out.write("\n")        
+        uni_out.close()    
     
 if __name__ == "__main__":
     usage = "%prog"
 
     parser = OptionParser(usage=usage)
     #parser.add_option('--method', action="store", help="Method for choosing slots/terms: {top_freq,bottom_freq,samp,rand}")
-    parser.add_option('--input', help="[Required] Input text file (tokenized).")
-    parser.add_option('--output', help="[Required] Output file")
+    parser.add_option('--in_text', help="[Required] Input text file (tokenized).")
+    parser.add_option('--out_counts', help="Output file for counts")
+    parser.add_option('--out_words', help="Output file for words only without counts")
     parser.add_option('--term_limit', action="store", type="int", default=-1, help="[Required] The max number of terms to include")
     parser.add_option('--min_occur', action="store", type="int", default=-1, help="The minimum number of occurences for a term to keep")
     #parser.add_option('--min_term_length', action="store", type="int", help="The min number of characters in a term (prior to stemming)")
