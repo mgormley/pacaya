90f41b095664d492b02cc0fe20c458bd6af02827
Matt Gormley <mrg@cs.jhu.edu>
2014-01-20 20:48:09 -0500

    Forgot to commit file.

---
+++ src/main/java/edu/jhu/gm/model/FeExpFamFactor.java
@@ -0,0 +1,27 @@
+package edu.jhu.gm.model;
+
+import edu.jhu.gm.feat.FeatureExtractor;
+import edu.jhu.gm.feat.FeatureVector;
+
+/**
+ * An exponential family factor which takes a FeatureExtractor at construction time and 
+ * uses it to extract features.
+ * 
+ * @author mgormley
+ */
+public class FeExpFamFactor extends ExpFamFactor {
+
+    private static final long serialVersionUID = 1L;
+    private FeatureExtractor fe;
+    
+    public FeExpFamFactor(VarSet vars, Object templateKey, FeatureExtractor fe) {
+        super(vars, templateKey);
+        this.fe = fe;
+    }
+
+    @Override
+    protected FeatureVector getFeatures(int configId) {
+        return fe.calcFeatureVector(this, configId);
+    }
+    
+}
