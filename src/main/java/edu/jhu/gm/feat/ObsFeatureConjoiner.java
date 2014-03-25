651c645216e99cc994571750198e83bf09b1ec85
Matt Gormley <mrg@cs.jhu.edu>
2014-03-25 12:47:14 -0400

    Moving StringIterable to standalone file

--- src/main/java/edu/jhu/gm/feat/ObsFeatureConjoiner.java
+++ src/main/java/edu/jhu/gm/feat/ObsFeatureConjoiner.java
@@ -360,42 +360,6 @@ public class ObsFeatureConjoiner implements Serializable {
         return Iterables.concat(new StringIterable(feAlphabet.getObjects()), new ParamNames());        
     }
     
-    private static class StringIterable implements Iterable<String>, Iterator<String>, Serializable{
-
-        private static final long serialVersionUID = 1L;
-        private Iterable<?> iterable;
-        private Iterator<?> iter;
-        public StringIterable(Iterable<?> iterable) {
-            this.iterable = iterable;
-        }
-        
-        @Override
-        public Iterator<String> iterator() { 
-            iter = iterable.iterator();
-            return this;
-        }
-
-        @Override
-        public boolean hasNext() {
-            return iter.hasNext();
-        }
-
-        @Override
-        public String next() {
-            Object next = iter.next();
-            if (next == null) {
-                return "null";
-            }   
-            return next.toString();
-        }
-
-        @Override
-        public void remove() {
-            iter.remove();
-        }
-        
-    }
-    
     private class ParamNames implements Iterable<String>, Iterator<String>, Serializable {
 
         private static final long serialVersionUID = 1L;
