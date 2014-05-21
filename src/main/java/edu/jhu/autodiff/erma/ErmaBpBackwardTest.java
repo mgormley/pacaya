fd7659d37c00e190d3a74a79e82b2631b9ee1fd9
Matt Gormley <mrg@cs.jhu.edu>
2014-05-21 17:06:39 -0400

    Adding additional tests of Erma and refactoring stochastic gradient approx class.

--- src/main/java/edu/jhu/autodiff/erma/ErmaBpBackwardTest.java
+++ src/main/java/edu/jhu/autodiff/erma/ErmaBpBackwardTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import com.apple.mrj.macos.carbon.Timer;
+
 import edu.jhu.autodiff.erma.ErmaBp.ErmaBpPrm;
 import edu.jhu.gm.inf.BeliefPropagation.BpScheduleType;
 import edu.jhu.gm.inf.BeliefPropagation.BpUpdateOrder;
@@ -12,7 +14,10 @@ import edu.jhu.gm.model.ExplicitFactor;
 import edu.jhu.gm.model.Factor;
 import edu.jhu.gm.model.FactorGraph;
 import edu.jhu.gm.model.FactorGraphTest;
+import edu.jhu.gm.model.Var;
+import edu.jhu.gm.model.VarSet;
 import edu.jhu.gm.model.FactorGraphTest.FgAndVars;
+import edu.jhu.gm.model.Var.VarType;
 import edu.jhu.gm.model.VarConfig;
 import edu.jhu.hlt.optimize.function.DifferentiableFunction;
 import edu.jhu.hlt.optimize.function.ValueGradient;
@@ -23,29 +28,109 @@ import edu.jhu.util.dist.Gaussian;
 
 
 public class ErmaBpBackwardTest {
+
+    private static boolean logDomain = false;
+    
+    @Test
+    public void testErmaGradientOneVar() {
+        FactorGraph fg = new FactorGraph();
+        Var t0 = new Var(VarType.PREDICTED, 2, "t0", null);
+        ExplicitFactor emit0 = new ExplicitFactor(new VarSet(t0)); 
+        fg.addFactor(emit0);
+                
+        VarConfig goldConfig = new VarConfig();
+        goldConfig.put(t0, 1);
+                
+        ErmaErFn fn = new ErmaErFn(fg, goldConfig); 
+        Prng.seed(12345);
+        fn.getGradient(getTheta(fn.getNumDimensions()));
+        
+        IntDoubleVector gradAd = testGradientByFiniteDifferences(fn);
+        //assertEquals(new double[]{-0.034560412986688674, 0.8756722814502975}, );
+    }
     
     // Tests ErmaBp and ExpectedRecall gradient by finite differences on a small chain factor graph.
     @Test
-    public void testGradientMatchesFiniteDifferences() {
-        ErmaErFn fn = new ErmaErFn();
+    public void testErmaGradientLinearChain() {
+        FgAndVars fgv = FactorGraphTest.getLinearChainFgWithVars(logDomain);
+        FactorGraph fg = fgv.fg;
         
+        VarConfig goldConfig = new VarConfig();
+        goldConfig.put(fgv.w0, 0);
+        goldConfig.put(fgv.w1, 1);
+        goldConfig.put(fgv.w2, 0);
+        goldConfig.put(fgv.t1, 1);
+        goldConfig.put(fgv.t2, 1);
+                
+        ErmaErFn fn = new ErmaErFn(fg, goldConfig);
+        
+        testGradientByFiniteDifferences(fn);
+    }
+    
+    @Test
+    public void testErmaGradientLinearChainWithLoops() {
+        FgAndVars fgv = FactorGraphTest.getLinearChainFgWithVars(logDomain);
+        FactorGraph fg = fgv.fg;
+        
+        ExplicitFactor loop0 = new ExplicitFactor(new VarSet(fgv.t0, fgv.t2)); 
+        ExplicitFactor loop1 = new ExplicitFactor(new VarSet(fgv.w0, fgv.w2)); 
+        ExplicitFactor loop2 = new ExplicitFactor(new VarSet(fgv.t0, fgv.t1, fgv.t2)); 
+        ExplicitFactor loop3 = new ExplicitFactor(new VarSet(fgv.w0, fgv.w1, fgv.w2)); 
+
+        fg.addFactor(loop0);
+        fg.addFactor(loop1);
+        fg.addFactor(loop2);
+        fg.addFactor(loop3);
+        
+        VarConfig goldConfig = new VarConfig();
+        goldConfig.put(fgv.w0, 0);
+        goldConfig.put(fgv.w1, 1);
+        goldConfig.put(fgv.w2, 0);
+        goldConfig.put(fgv.t1, 1);
+        goldConfig.put(fgv.t2, 1);
+                
+        ErmaBpPrm prm = new ErmaBpPrm();
+        prm.updateOrder = BpUpdateOrder.PARALLEL;
+        prm.maxIterations = 10;
+        prm.logDomain = logDomain;
+        prm.normalizeMessages = true;
+        
+        ErmaErFn fn = new ErmaErFn(fg, goldConfig, prm);
+        
+        testGradientByFiniteDifferences(fn);
+    }
+
+    private static IntDoubleVector testGradientByFiniteDifferences(ErmaErFn fn) {
         Prng.seed(12345);
         int numParams = fn.getNumDimensions();
         IntDoubleVector theta0 = getTheta(numParams);
+        System.out.println("theta0 = " + theta0);
         Prng.seed(System.currentTimeMillis());
         
-        IntDoubleVector gradFd = StochasticGradientApproximation.estimateGradient(fn, theta0);      
+        IntDoubleVector gradFd0 = StochasticGradientApproximation.estimateGradientSpsa(fn, theta0, 1000);      
+        IntDoubleVector gradFd1 = StochasticGradientApproximation.estimateGradientSpsa(fn, theta0, 1000);      
         IntDoubleVector gradAd = fn.getGradient(theta0);
-        System.out.println(gradFd);
-        System.out.println(gradAd);
+        System.out.println("gradFd = " + gradFd0);
+        System.out.println("gradFd = " + gradFd1);
+        System.out.println("gradAd = " + gradAd);
         
-        IntDoubleDenseVector diff = new IntDoubleDenseVector(gradFd);
-        diff.subtract(gradAd);
+        double infNormFd = infNorm(gradFd0, gradFd1);
+        double infNorm = infNorm(gradFd0, gradAd);
+        System.out.println("infNorm(gradFd0, gradFd1) = " + infNormFd);
+        System.out.println("infNorm(gradFd0, gradAd) = " + infNorm);
+        assertTrue(infNorm < 1e-1);
         
-        assertTrue(diff.infinityNorm() < 1e-1);
+        return gradAd;
     }
 
-    private IntDoubleDenseVector getTheta(int numParams) {
+    private static double infNorm(IntDoubleVector gradFd0, IntDoubleVector gradAd) {
+        IntDoubleDenseVector diff = new IntDoubleDenseVector(gradFd0);
+        diff.subtract(gradAd);        
+        double infNorm = diff.infinityNorm();
+        return infNorm;
+    }
+
+    private static IntDoubleDenseVector getTheta(int numParams) {
         // Define the "model" as the explicit factor entries.
         IntDoubleDenseVector theta = new IntDoubleDenseVector(numParams);
         // Randomly initialize the model.
@@ -57,22 +142,35 @@ public class ErmaBpBackwardTest {
     
     private static class ErmaErFn implements DifferentiableFunction {
 
-        boolean logDomain = false;
         private FactorGraph fg;
         private VarConfig goldConfig;
         private int numParams;
+        private ErmaBp bp;
+        private ExpectedRecall er;
+        private ErmaBpPrm prm;
+        
+        public ErmaErFn(FactorGraph fg, VarConfig goldConfig) {
+            this(fg, goldConfig, getDefaultErmaBpPrm());
+        }
 
-        public ErmaErFn() {
-            FgAndVars fgv = FactorGraphTest.getLinearChainFgWithVars(logDomain);
-            fg = fgv.fg;
-            
-            goldConfig = new VarConfig();
-            goldConfig.put(fgv.w0, 0);
-            goldConfig.put(fgv.w1, 1);
-            goldConfig.put(fgv.w2, 0);
-            goldConfig.put(fgv.t1, 1);
-            goldConfig.put(fgv.t2, 1);
-            
+        public ErmaErFn(FactorGraph fg, VarConfig goldConfig, ErmaBpPrm prm) {
+            this.fg = fg;
+            this.goldConfig = goldConfig;
+            this.prm = prm;
+            cacheNumParams();
+        }
+
+        private static ErmaBpPrm getDefaultErmaBpPrm() {
+            ErmaBpPrm prm = new ErmaBpPrm();
+            prm.updateOrder = BpUpdateOrder.SEQUENTIAL;
+            prm.schedule = BpScheduleType.TREE_LIKE;
+            prm.maxIterations = 1;
+            prm.logDomain = logDomain;
+            prm.normalizeMessages = true;
+            return prm;
+        }
+
+        private void cacheNumParams() {
             numParams = 0;
             for (Factor f : fg.getFactors()) {
                 numParams += f.getVars().calcNumConfigs();
@@ -81,34 +179,7 @@ public class ErmaBpBackwardTest {
         
         @Override
         public double getValue(IntDoubleVector theta) {
-            updateFactorGraphFromModel(theta);
-            return getExpectedRecall(logDomain, fg, goldConfig);
-        }
-
-        private void updateFactorGraphFromModel(IntDoubleVector theta) {
-            // Update factors from the model in params array.
-            int i=0;
-            for (Factor f : fg.getFactors()) {
-                ExplicitFactor factor = (ExplicitFactor) f;
-                for (int c=0; c<factor.size(); c++) {
-                    factor.setValue(c, theta.get(i++));
-                }
-            }
-        }
-
-        private double getExpectedRecall(boolean logDomain, FactorGraph fg, VarConfig goldConfig) {
-            ErmaBpPrm prm = new ErmaBpPrm();
-            prm.updateOrder = BpUpdateOrder.SEQUENTIAL;
-            prm.schedule = BpScheduleType.TREE_LIKE;
-            prm.logDomain = logDomain;
-            prm.normalizeMessages = true;
-            ErmaBp bp = new ErmaBp(fg, prm);
-            bp.forward();
-            
-            ExpectedRecall er = new ExpectedRecall(bp, goldConfig);
-            er.forward();
-            
-            return er.getExpectedRecall();
+            return runForward(theta);
         }
         
         @Override
@@ -118,18 +189,8 @@ public class ErmaBpBackwardTest {
 
         /** Get the gradient by running AD. */
         @Override
-        public IntDoubleVector getGradient(IntDoubleVector point) {
-            ErmaBpPrm prm = new ErmaBpPrm();
-            prm.updateOrder = BpUpdateOrder.SEQUENTIAL;
-            prm.schedule = BpScheduleType.TREE_LIKE;
-            prm.logDomain = logDomain;
-            prm.normalizeMessages = true;
-            ErmaBp bp = new ErmaBp(fg, prm);
-            bp.forward();
-            
-            ExpectedRecall er = new ExpectedRecall(bp, goldConfig);
-            er.forward();
-            
+        public IntDoubleVector getGradient(IntDoubleVector theta) {
+            runForward(theta);
             er.backward(1);
             bp.backward(er.getVarBeliefsAdjs(), er.getFacBeliefsAdjs());
             
@@ -148,6 +209,26 @@ public class ErmaBpBackwardTest {
         @Override
         public ValueGradient getValueGradient(IntDoubleVector point) {
             return new ValueGradient(getValue(point), getGradient(point));
+        }
+
+        private double runForward(IntDoubleVector theta) {
+            updateFactorGraphFromModel(theta);
+            bp = new ErmaBp(fg, prm);
+            bp.forward();            
+            er = new ExpectedRecall(bp, goldConfig);
+            er.forward();
+            return er.getExpectedRecall();
+        }
+
+        private void updateFactorGraphFromModel(IntDoubleVector theta) {
+            // Update factors from the model in params array.
+            int i=0;
+            for (Factor f : fg.getFactors()) {
+                ExplicitFactor factor = (ExplicitFactor) f;
+                for (int c=0; c<factor.size(); c++) {
+                    factor.setValue(c, theta.get(i++));
+                }
+            }
         }
         
     }
--- src/main/java/edu/jhu/autodiff/erma/StochasticGradientApproximation.java
+++ src/main/java/edu/jhu/autodiff/erma/StochasticGradientApproximation.java
@@ -10,45 +10,65 @@ import edu.jhu.util.Prng;
 import edu.jhu.util.dist.Gaussian;
 
 public class StochasticGradientApproximation {
-
-    public static IntDoubleVector estimateGradient(Function fn, IntDoubleVector theta0) {
+    
+    /**
+     * Estimates a gradient of a function by simultaneous perterbations
+     * stochastic approximation (SPSA) (Spall, 1992).
+     * 
+     * @param fn Function on which to approximate the gradient.
+     * @param x Point at which to approximate the gradient.
+     * @param numSamples Number of samples to take, which will be averaged
+     *            together (typically 1).
+     * @return The gradient approximation.
+     */
+    public static IntDoubleVector estimateGradientSpsa(Function fn, IntDoubleVector x, int numSamples) {
         int numParams = fn.getNumDimensions();
         // The gradient estimate
         IntDoubleVector grad = new IntDoubleDenseVector(numParams);
         // The scaling term
-        double c = 0.00001;
-        // The dimension
-        double p = numParams;
-        // The number of samples
-        int numSamples = 1000;
+        double c = 1e-6;
         for (int k=0; k<numSamples; k++) {   
             // Get a random direction
-            IntDoubleVector e0 = getRandomBernoulliDirection(numParams);
-            // Compute ( L(\theta + c * e) - L(\theta - c * e) ) / (2c)
-            double scaler = 0;
-            {
-                // L(\theta + c * e)
-                IntDoubleVector e = e0.copy();
-                e.scale(c);
-                IntDoubleVector theta = theta0.copy();
-                theta.add(e);
-                scaler += fn.getValue(theta);
-            }
-            {
-                // - L(\theta - c * e)
-                IntDoubleVector e = e0.copy();
-                e.scale(-c);
-                IntDoubleVector theta = theta0.copy();
-                theta.add(e);        
-                scaler -= fn.getValue(theta);
-            }
-            scaler /= 2.0 * c;
+            IntDoubleVector d = getRandomBernoulliDirection(numParams);
+            double scaler = getGradDotDirApprox(fn, x, d, c);
             for (int i=0; i< numParams; i++) {
-                grad.add(i, scaler * 1.0 / e0.get(i));
+                grad.add(i, scaler * 1.0 / d.get(i));
             }
         }
         grad.scale(1.0 / numSamples);
         return grad;
+    }
+
+    /**
+     * Compute f'(x)^T d = ( L(x + c * d) - L(x - c * d) ) / (2c)
+     * 
+     * @param fn Function, f.
+     * @param x Point at which to evaluate the gradient, x.
+     * @param d Random direction, d.
+     * @param c Epsilon constant.
+     * @return
+     */
+    private static double getGradDotDirApprox(Function fn, IntDoubleVector x, IntDoubleVector d, double c) {
+        // 
+        double scaler = 0;
+        {
+            // L(\theta + c * d)
+            IntDoubleVector d1 = d.copy();
+            d1.scale(c);
+            IntDoubleVector x1 = x.copy();
+            x1.add(d1);
+            scaler += fn.getValue(x1);
+        }
+        {
+            // - L(\theta - c * d)
+            IntDoubleVector d1 = d.copy();
+            d1.scale(-c);
+            IntDoubleVector x1 = x.copy();
+            x1.add(d1);        
+            scaler -= fn.getValue(x1);
+        }
+        scaler /= 2.0 * c;
+        return scaler;
     }
     
     /* ----------- For random directions ------------ */
@@ -67,20 +87,20 @@ public class StochasticGradientApproximation {
         for (int i=0; i<p; i++) {
             e.set(i, Gaussian.nextDouble(0, 1));
         }
-        scaleL2NormToValue(e, p);
+        scaleL2NormToValue(e, 1.0);
         return e;
     }
 
-    public static void scaleL2NormToValue(IntDoubleVector e, int desiredL2) {
-        double curL2 = getSumOfSquares(e);
-        e.scale(FastMath.sqrt(desiredL2 / curL2));
-        assert Primitives.equals(getSumOfSquares(e), desiredL2, 1e-13); 
+    public static void scaleL2NormToValue(IntDoubleVector d, double desiredL2) {
+        double curL2 = getSumOfSquares(d);
+        d.scale(FastMath.sqrt(desiredL2 / curL2));
+        assert Primitives.equals(getSumOfSquares(d), desiredL2, 1e-13); 
     }
 
-    public static double getSumOfSquares(IntDoubleVector e) {
+    public static double getSumOfSquares(IntDoubleVector d) {
         double curL2 = 0;
-        for (int i=0; i<e.getNumImplicitEntries(); i++) {
-            curL2 += e.get(i) * e.get(i);
+        for (int i=0; i<d.getNumImplicitEntries(); i++) {
+            curL2 += d.get(i) * d.get(i);
         }
         return curL2;
     }
