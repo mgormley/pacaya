e86687f4722556c1155ada5f36d470ca5d360b3b
Matt Gormley <mrg@cs.jhu.edu>
2014-05-21 00:58:30 -0400

    Switching from double[]s to IntDoubleVectors

--- src/main/java/edu/jhu/autodiff/erma/ErmaBpBackwardTest.java
+++ src/main/java/edu/jhu/autodiff/erma/ErmaBpBackwardTest.java
@@ -8,22 +8,25 @@ import org.junit.Test;
 import edu.jhu.autodiff.erma.ErmaBp.ErmaBpPrm;
 import edu.jhu.gm.inf.BeliefPropagation.BpScheduleType;
 import edu.jhu.gm.inf.BeliefPropagation.BpUpdateOrder;
-import edu.jhu.gm.model.DenseFactor;
 import edu.jhu.gm.model.ExplicitFactor;
 import edu.jhu.gm.model.Factor;
 import edu.jhu.gm.model.FactorGraph;
 import edu.jhu.gm.model.FactorGraphTest;
 import edu.jhu.gm.model.FactorGraphTest.FgAndVars;
 import edu.jhu.gm.model.VarConfig;
-import edu.jhu.hlt.util.math.Vectors;
+import edu.jhu.hlt.optimize.function.DifferentiableFunction;
+import edu.jhu.hlt.optimize.function.ValueGradient;
 import edu.jhu.prim.Primitives;
 import edu.jhu.prim.arrays.DoubleArrays;
+import edu.jhu.prim.vector.IntDoubleDenseVector;
+import edu.jhu.prim.vector.IntDoubleVector;
 import edu.jhu.util.Prng;
 import edu.jhu.util.dist.Gaussian;
 
 
 public class ErmaBpBackwardTest {
 	    
+    
 	@Test
 	public void testGradientMatchesFiniteDifferences() {
 	    
@@ -39,46 +42,47 @@ public class ErmaBpBackwardTest {
         goldConfig.put(fgv.t2, 1);
         
         Prng.seed(12345);
-	    double[] theta0 = getTheta(fg);
-	    Prng.seed(System.currentTimeMillis());
+	    IntDoubleVector theta0 = new IntDoubleDenseVector();
+	    int numParams = getTheta(fg, theta0);
+        Prng.seed(System.currentTimeMillis());
 	    
 	    // The gradient estimate
-	    double[] grad = new double[theta0.length];
+	    IntDoubleVector grad = new IntDoubleDenseVector(numParams);
 	    // The scaling term
 	    double c = 0.00001;
         // The dimension
-        double p = theta0.length;
+        double p = numParams;
         // The number of samples
         int numSamples = 1000;
 	    for (int k=0; k<numSamples; k++) {	 
             // Get a random direction
-            double[] e0 = getRandomBernoulliDirection(theta0.length);
+            IntDoubleVector e0 = getRandomBernoulliDirection(numParams);
             // Compute ( L(\theta + c * e) - L(\theta - c * e) ) / (2c)
             double scaler = 0;
             {
                 // L(\theta + c * e)
-                double[] e = DoubleArrays.copyOf(e0);
-                DoubleArrays.scale(e, c);
-                double[] theta = DoubleArrays.copyOf(theta0);
-                DoubleArrays.add(theta, e);	        
+                IntDoubleVector e = e0.copy();
+                e.scale(c);
+                IntDoubleVector theta = theta0.copy();
+                theta.add(e);
                 scaler += getExpectedRecall(theta, logDomain, fg, goldConfig);
             }
             {
                 // - L(\theta - c * e)
-                double[] e = DoubleArrays.copyOf(e0);
-                DoubleArrays.scale(e, -c);
-                double[] theta = DoubleArrays.copyOf(theta0);
-                DoubleArrays.add(theta, e);         
+                IntDoubleVector e = e0.copy();
+                e.scale(-c);
+                IntDoubleVector theta = theta0.copy();
+                theta.add(e);        
                 scaler -= getExpectedRecall(theta, logDomain, fg, goldConfig);
             }
             scaler /= 2.0 * c;
-            for (int i=0; i<grad.length; i++) {
-                grad[i] += scaler * 1.0 / e0[i];
+            for (int i=0; i< numParams; i++) {
+                grad.add(i, scaler * 1.0 / e0.get(i));
             }
 	    }
-	    DoubleArrays.scale(grad, 1.0 / numSamples);
+	    grad.scale(1.0 / numSamples);
 	    
-	    System.out.println(Arrays.toString(grad));
+	    System.out.println(grad);
 	    
 //	    er.backward(1);
 //	    bp.backward(er.getVarBeliefsAdjs(), er.getFacBeliefsAdjs());
@@ -89,33 +93,33 @@ public class ErmaBpBackwardTest {
 //	    }
 	}
 
-    private double getExpectedRecall(double[] theta, boolean logDomain, FactorGraph fg, VarConfig goldConfig) {
+    private int getTheta(FactorGraph fg, IntDoubleVector theta) {
+        // Define the "model" as the explicit factor entries.
+        int numParams = 0;
+        for (Factor f : fg.getFactors()) {
+            numParams += f.getVars().calcNumConfigs();
+        }
+        // Randomly initialize the model.
+        for (int i=0; i< numParams; i++) {
+            theta.set(i, Math.abs(Gaussian.nextDouble(0.0, 1.0)));
+        }
+        return numParams;
+    }
+
+
+    private double getExpectedRecall(IntDoubleVector theta, boolean logDomain, FactorGraph fg, VarConfig goldConfig) {
         // Update factors from the model in params array.
 	    int i=0;
 	    for (Factor f : fg.getFactors()) {
 	        ExplicitFactor factor = (ExplicitFactor) f;
             for (int c=0; c<factor.size(); c++) {
-                factor.setValue(c, theta[i++]);
+                factor.setValue(c, theta.get(i++));
             }
         }
 	    
 	    return getExpectedRecall(logDomain, fg, goldConfig);
 	}
-
-    private double[] getTheta(FactorGraph fg) {
-        // Define the "model" as the explicit factor entries.
-	    int numParams = 0;
-	    for (Factor f : fg.getFactors()) {
-	        numParams += f.getVars().calcNumConfigs();
-	    }
-	    // Randomly initialize the model.
-	    double[] theta = new double[numParams];
-	    for (int i=0; i<theta.length; i++) {
-	        theta[i] = Math.abs(Gaussian.nextDouble(0.0, 1.0));
-	    }
-        return theta;
-    }
-
+    
     private double getExpectedRecall(boolean logDomain, FactorGraph fg, VarConfig goldConfig) {
         ErmaBpPrm prm = new ErmaBpPrm();
 	    prm.updateOrder = BpUpdateOrder.SEQUENTIAL;
@@ -134,34 +138,34 @@ public class ErmaBpBackwardTest {
     /* ----------- For random directions ------------ */
     // TODO: Move to separate class
     
-    public static double[] getRandomBernoulliDirection(int p) {
-        double[] e = new double[p];
-        for (int i=0; i<e.length; i++) {
+    public static IntDoubleVector getRandomBernoulliDirection(int p) {
+        IntDoubleVector e = new IntDoubleDenseVector(p);
+        for (int i=0; i<p; i++) {
             // Bernoulli distribution chooses either positive or negative 1.
-            e[i] = (Prng.nextBoolean()) ? 1 : -1;
+            e.set(i, (Prng.nextBoolean()) ? 1 : -1);
         }
         return e;
     }
 
-    public static double[] getRandomGaussianDirection(int p) {
-        double[] e = new double[p];
-        for (int i=0; i<e.length; i++) {
-            e[i] = Gaussian.nextDouble(0, 1);
+    public static IntDoubleVector getRandomGaussianDirection(int p) {
+        IntDoubleVector e = new IntDoubleDenseVector(p);
+        for (int i=0; i<p; i++) {
+            e.set(i, Gaussian.nextDouble(0, 1));
         }
         scaleL2NormToValue(e, p);
         return e;
     }
 
-    public static void scaleL2NormToValue(double[] e, int desiredL2) {
+    public static void scaleL2NormToValue(IntDoubleVector e, int desiredL2) {
         double curL2 = getSumOfSquares(e);
-        DoubleArrays.scale(e, FastMath.sqrt(desiredL2 / curL2));
+        e.scale(FastMath.sqrt(desiredL2 / curL2));
         assert Primitives.equals(getSumOfSquares(e), desiredL2, 1e-13); 
     }
 
-    public static double getSumOfSquares(double[] e) {
+    public static double getSumOfSquares(IntDoubleVector e) {
         double curL2 = 0;
-        for (int i=0; i<e.length; i++) {
-            curL2 += e[i] * e[i];
+        for (int i=0; i<e.getNumImplicitEntries(); i++) {
+            curL2 += e.get(i) * e.get(i);
         }
         return curL2;
     }
