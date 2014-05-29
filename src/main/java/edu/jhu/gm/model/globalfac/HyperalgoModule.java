ce476a4f390e370c244506a5d9093b48108b3ebf
Matt Gormley <mrg@cs.jhu.edu>
2014-05-29 12:11:22 -0400

    Moving HyperalgoModule to new file

---
+++ src/main/java/edu/jhu/gm/model/globalfac/HyperalgoModule.java
@@ -0,0 +1,112 @@
+package edu.jhu.gm.model.globalfac;
+
+import java.util.List;
+
+import edu.jhu.autodiff.AbstractTensorModule;
+import edu.jhu.autodiff.Module;
+import edu.jhu.autodiff.Tensor;
+import edu.jhu.hypergraph.Hyperalgo;
+import edu.jhu.hypergraph.Hyperedge;
+import edu.jhu.hypergraph.Hypernode;
+import edu.jhu.hypergraph.Hyperpotential;
+import edu.jhu.hypergraph.Hyperalgo.HyperedgeDoubleFn;
+import edu.jhu.hypergraph.Hyperalgo.Scores;
+import edu.jhu.hypergraph.depparse.FirstOrderDepParseHypergraph;
+import edu.jhu.hypergraph.depparse.FirstOrderDepParseHypergraph.PCBasicHypernode;
+import edu.jhu.parse.dep.EdgeScores;
+import edu.jhu.util.collections.Lists;
+import edu.jhu.util.semiring.Algebra;
+
+public class HyperalgoModule extends AbstractTensorModule implements Module<Tensor> {
+    
+    public static final int BETA_IDX = 0;
+    public static final int ALPHA_IDX = 1;
+    public static final int ROOT_IDX = 2;
+    
+    private Module<Tensor> weightsIn;
+    private Algebra s; // TODO: semiring, use when filling yAdj in abstract parent.
+    
+    private Scores scores;
+    // Cached for efficiency.
+    private FirstOrderDepParseHypergraph graph;
+    
+    public HyperalgoModule(Module<Tensor> weightsIn, Algebra s) {           
+        this.weightsIn = weightsIn;
+        this.s = s;
+    }
+    
+    @Override
+    public Tensor forward() {
+        EdgeScores es = EdgeScores.tensorToEdgeScores(weightsIn.getOutput());            
+        scores = new Scores();
+        graph = new FirstOrderDepParseHypergraph(es.root, es.child, s);            
+        Hyperpotential w = graph.getPotentials();
+        Hyperalgo.insideAlgorithm(graph, w, s, scores);
+        Hyperalgo.outsideAlgorithm(graph, w, s, scores);
+        int n = graph.getNumTokens();
+        
+        y = new Tensor(3, n, n);
+        y.fill(s.zero());
+        for (Hypernode node : graph.getNodes()) {
+            if (node instanceof PCBasicHypernode) {   
+                PCBasicHypernode pc = (PCBasicHypernode) node;
+                int p = pc.getP();
+                int c = pc.getC();
+                int pp = EdgeScores.getTensorParent(n, p, c);
+                
+                int id = node.getId();
+                y.set(scores.beta[id], BETA_IDX, pp, c);
+                y.set(scores.alpha[id], ALPHA_IDX, pp, c);
+            }
+        }
+        y.set(scores.beta[graph.getRoot().getId()], ROOT_IDX, 0, 0);
+        return y;
+    }
+
+    @Override
+    public void backward() {
+        final int n = graph.getNumTokens();
+        // Update output adjoints in scores.
+        for (Hypernode node : graph.getNodes()) {
+            if (node instanceof PCBasicHypernode) {   
+                PCBasicHypernode pc = (PCBasicHypernode) node;
+                int p = pc.getP();
+                int c = pc.getC();
+                int pp = EdgeScores.getTensorParent(n, p, c);
+                
+                int id = node.getId();
+                scores.alphaAdj[id] = yAdj.get(ALPHA_IDX, pp, c);
+                scores.betaAdj[id] = yAdj.get(BETA_IDX, pp, c);
+            }
+        }
+        scores.betaAdj[graph.getRoot().getId()] = yAdj.get(ROOT_IDX, 0, 0);
+
+        // Run backward pass.
+        Hyperpotential w = graph.getPotentials();
+        Hyperalgo.outsideAdjoint(graph, w, s, scores);
+        Hyperalgo.insideAdjoint(graph, w, s, scores);
+        Hyperalgo.weightAdjoint(graph, w, s, scores);
+        
+        // Update input adjoints on weightsIn.
+        final Tensor wAdj = weightsIn.getOutputAdj();
+        HyperedgeDoubleFn lambda = new HyperedgeDoubleFn() {
+            public void apply(Hyperedge e, double adj_w_e) {
+                Hypernode head = e.getHeadNode();
+                if (head instanceof PCBasicHypernode) {   
+                    PCBasicHypernode pc = (PCBasicHypernode) head;
+                    int p = pc.getP();
+                    int c = pc.getC();
+                    int pp = EdgeScores.getTensorParent(n, p, c);
+                    wAdj.add(adj_w_e, pp, c);
+                }
+            }
+        };
+        Hyperalgo.weightAdjoint(graph, w, s, scores, lambda);
+    }
+
+    @Override
+    public List<Module<Tensor>> getInputs() {
+        return Lists.getList(weightsIn);
+    }
+    
+}
