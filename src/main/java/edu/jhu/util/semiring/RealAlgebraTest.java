4fc7a2677853ff12b5662c004670d82a49c756f5
Matt Gormley <mrg@cs.jhu.edu>
2014-08-07 13:37:11 -0400

    Factoring out AbstractAlgebraTest from LogSignAlgebraTest

--- src/main/java/edu/jhu/util/semiring/RealAlgebraTest.java
+++ src/main/java/edu/jhu/util/semiring/RealAlgebraTest.java
@@ -4,13 +4,18 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class RealAlgebraTest {
+public class RealAlgebraTest extends AbstractAlgebraTest {
 
     @Test
     public void testEquals() {
         RealAlgebra s1 = new RealAlgebra();
         RealAlgebra s2 = new RealAlgebra();
         assertTrue(s1.equals(s2));
+    }
+
+    @Override
+    public Algebra getAlgebra() {
+        return new RealAlgebra();
     }
 
 }
