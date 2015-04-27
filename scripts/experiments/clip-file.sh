0dc8c3b6a28a390e9d7728750357cdf8904daf13
Matt Gormley <mrg@cs.jhu.edu>
2015-04-27 11:02:22 -0400

    Removed scripts/ directory

--- scripts/experiments/clip-file.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Clips out the middle of a file and writes the new version to a file with the .clip extension.
-#
-# Example usage: 
-# find archive_acl_13/bnb_005/ -name "stdout" | xargs -n 1 ../scripts/experiments/clip-file.sh
-#
-
-(head -n 1000; tail -n 1000) < $1 > $1.clip
