5616eb0d74f3f0c20ad0c79cdfd85e7930bcdef7
Matt Gormley <mrg@cs.jhu.edu>
2015-11-03 17:10:08 -0500

    Bug fix: tests weren't calling ReporterManager.init()

--- src/main/java/edu/jhu/pacaya/util/report/ReporterManager.java
+++ src/main/java/edu/jhu/pacaya/util/report/ReporterManager.java
@@ -6,22 +6,30 @@ import java.io.FileOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import edu.jhu.pacaya.util.cli.Opt;
 
 public class ReporterManager {
-    
+
+    private static final Logger log = LoggerFactory.getLogger(ReporterManager.class);
+
     @Opt(hasArg = true, description = "File to which to which reports should be written.")
     public static File reportOut = null;
     
     private static Map<Class<?>,Reporter> reps = new HashMap<>();
     private static StreamReporter wr;
-    private static boolean useLog4j;
+    private static boolean useLogger;
     private static boolean initialized = false;
     
     private ReporterManager() { }
     
-    public static void init(File reportOut, boolean useLog4j) {
-        ReporterManager.useLog4j = useLog4j;
+    public static void init(File reportOut, boolean useLogger) {
+        if (initialized) {
+            log.warn("ReporterManager.init() is being called twice.");
+        }
+        ReporterManager.useLogger = useLogger;
         if (wr != null) {
             throw new RuntimeException("setReportFile() may only be called once");
         }
@@ -33,12 +41,14 @@ public class ReporterManager {
             throw new RuntimeException(e);
         }
         initialized = true;
-        // Update reporters that were created before initialization.
-        for (Class<?> clazz : reps.keySet()) {
-            InitReporter rl = (InitReporter) reps.get(clazz);
-            rl.set(getReporter(clazz));
+        if (reps != null) {
+            // Update reporters that were created before initialization.
+            for (Class<?> clazz : reps.keySet()) {
+                InitReporter rep = (InitReporter) reps.get(clazz);
+                rep.set(getReporter(clazz));
+            }
+            reps = null;
         }
-        reps = null;
     }
     
     public static Reporter getReporter(Class<?> clazz) {
@@ -46,11 +56,11 @@ public class ReporterManager {
         if (!initialized) {
             r = new InitReporter();
             reps.put(clazz, r);
-        } else if (useLog4j && wr != null) {
+        } else if (useLogger && wr != null) {
             r = new ReporterList(new Log4jReporter(clazz), wr);
-        } else if (useLog4j && wr == null) {
+        } else if (useLogger && wr == null) {
             r = new Log4jReporter(clazz);
-        } else if (!useLog4j && wr != null) {
+        } else if (!useLogger && wr != null) {
             r = wr;
         } else {
             r = new ReporterList();
